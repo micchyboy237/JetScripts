4e4b41f4251bdb2cec1693e524993ab2e15d796e
Jethro Estrada <jethroestrada237@gmail.com>
2025-01-07 06:10:16 +0800

    Updates

--- python_modules/script_utils/llama_index_utils.py
+++ python_modules/script_utils/llama_index_utils.py
@@ -2,33 +2,10 @@ from typing import TypedDict, Optional, Union
 import json
 from llama_index.core.schema import BaseNode, TextNode, ImageNode, NodeWithScore
 from llama_index.core.base.response.schema import Response
+from jet.vectors import SourceNodeAttributes
 from jet.transformers import make_serializable
 from jet.utils import check_object_type
 from jet.logger import logger
-
-
-class TextNodeAttributes(TypedDict, total=False):
-    text: str
-    text_length: int
-    start_end: tuple[int, int]
-
-
-class ImageNodeAttributes(TypedDict, total=False):
-    image: str
-    image_path: str
-    image_url: str
-    image_mimetype: str
-
-
-class SourceNodeAttributes(TypedDict):
-    node_id: str
-    metadata: Optional[dict]
-    score: Optional[float]
-    similarity: Optional[float]
-    text: Optional[str]
-    text_length: Optional[int]
-    start_end: Optional[tuple[int, int]]
-    image_info: Optional[ImageNodeAttributes]
 
 
 def display_source_node(
@@ -105,45 +82,6 @@ def display_source_nodes(
         )
         logger.log("---")
     logger.newline()
-
-
-def get_source_node_attributes(
-    source_node: NodeWithScore | BaseNode | Response,
-) -> SourceNodeAttributes:
-    attributes: SourceNodeAttributes = {}
-
-    score = None
-
-    if hasattr(source_node, "score"):
-        score = source_node.score
-        attributes["score"] = score
-
-    if isinstance(source_node.node, BaseNode):
-        source_node = source_node.node
-
-    attributes["node_id"] = source_node.node_id
-    attributes["metadata"] = source_node.metadata
-
-    if isinstance(source_node, TextNode):
-        attributes["text"] = source_node.text
-        attributes["text_length"] = len(source_node.text)
-
-        if hasattr(source_node, "start_char_idx") and hasattr(source_node, "end_char_idx"):
-            attributes["start_end"] = (
-                source_node.start_char_idx, source_node.end_char_idx)
-
-    if isinstance(source_node, ImageNode):
-        attributes["image_info"] = {
-            "image": source_node.image,
-            "image_path": source_node.image_path,
-            "image_url": source_node.image_url,
-            "image_mimetype": source_node.image_mimetype,
-        }
-
-    if score is not None:
-        attributes["similarity"] = score
-
-    return attributes
 
 
 # Example usage
