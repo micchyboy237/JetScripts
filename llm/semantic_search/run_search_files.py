d30ea91322e875dc47aaf79313a4517f7e1436b6
Jethro Estrada <jethroestrada237@gmail.com>
2025-01-06 22:24:03 +0800

    Updates

--- llm/semantic_search/run_search_files.py
+++ llm/semantic_search/run_search_files.py
@@ -1,65 +1,130 @@
-import os
-from pathlib import Path
 import shutil
-from typing import Generator
-from jet.code.markdown_code_extractor import MarkdownCodeExtractor
-from llama_index.core.node_parser import SentenceSplitter
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, PromptTemplate
-from llama_index.core.query_engine import RetrieverQueryEngine
-from llama_index.core.base.base_retriever import BaseRetriever
-from llama_index.core.retrievers import QueryFusionRetriever
+from llama_index.core import Document, SimpleDirectoryReader, PromptTemplate
 from llama_index.core.retrievers.fusion_retriever import FUSION_MODES
-from llama_index.core.schema import Document, NodeWithScore, BaseNode, TextNode, ImageNode
+
+from convert_docs_to_scripts import scrape_code
 from script_utils import display_source_nodes
-from jet.llm.llm_types import OllamaChatOptions
+from jet.llm.token import token_counter
 from jet.llm.query import setup_index, query_llm
-from jet.llm import call_ollama_chat
-from jet.utils import search_files
 from jet.logger import logger
-from jet.llm.ollama import initialize_ollama_settings, large_llm_model
-initialize_ollama_settings()
+from jet.llm.ollama import initialize_ollama_settings, small_embed_model
+initialize_ollama_settings(settings={"embedding_model": small_embed_model})
+
+
+def get_token_counts(texts, model):
+    token_counts = token_counter(
+        texts, model, prevent_total=True)
+    max_count = max(token_counts)
+    min_count = min(token_counts)
+    total_count = sum(token_counts)
+    return {
+        "max": max_count,
+        "min": min_count,
+        "total": total_count,
+    }
 
 
 if __name__ == "__main__":
-    system = (
-        "You are a job applicant providing tailored responses during an interview.\n"
-        "Always answer questions using the provided context as if it is your resume, "
-        "and avoid referencing the context directly.\n"
-        "Some rules to follow:\n"
-        "1. Never directly mention the context or say 'According to my resume' or similar phrases.\n"
-        "2. Provide responses as if you are the individual described in the context, focusing on professionalism and relevance."
-    )
+    model = "llama3.1"
+    max_tokens = 0.5
+    score_threshold = 0.2
+
+    code_types = ["python", "text"]
+    # sample_query = "Write real world usage example for tree summarizer with complete code and a main function. Respond only with the generated code as a single large code block."
+    # sample_query = "Write usage examples for for sql_agent, ToolMetadata and OpenAIAgent."
+    sample_query = "Write real world usage example for tree summarizer."
+    # sample_query = "Create endpoints for the main features."
+
+    results_dir = "generated/results"
+
+    system = "You are an AI assistant that follows instructions. You can refactor code, understand and write code in any programming language, and extract code from unstructured web content. You are skilled in fixing bugs and syntax errors while ensuring the code is clean, optimized, readable, and modular. You explain code clearly and systematically using markdown, providing real-world usage examples to enhance understanding and usability."
 
     prompt_template = PromptTemplate(
         """\
-    Resume details are below.
+    Context information are below.
     ---------------------
     {context_str}
     ---------------------
-    Given the resume details and not prior knowledge, respond to the question.
+    Given the context information and not prior knowledge, respond to the question.
     Question: {query_str}
     Response: \
     """
     )
 
     data_dir = "/Users/jethroestrada/Desktop/External_Projects/AI/repo-libs/llama_index/docs/docs/understanding/putting_it_all_together"
-    extensions = [".ipynb", ".md", ".mdx", ".rst"]
+    data_extensions = [".ipynb", ".md", ".mdx", ".rst"]
     rag_dir = "generated/rag"
+    rag_extensions = [".py"]
+
+    shutil.rmtree(rag_dir, ignore_errors=True)
 
     include_files = []
     exclude_files = []
 
-    # rag_files = search_files(data_dir, extensions,
+    # rag_files = search_files(rag_dir, extensions,
     #                          include_files, exclude_files)
     # logger.info(f"Found {len(rag_files)} files with extensions {extensions}")
 
-    results_dir = "generated/results"
-    sample_query = "How to start"
+    results = scrape_code(
+        data_dir,
+        data_extensions,
+        include_files=include_files,
+        exclude_files=exclude_files,
+        with_markdown=True,
+        with_ollama=True,
+        output_base_dir=rag_dir,
+        types=code_types,
+    )
 
-    documents = SimpleDirectoryReader(
-        rag_dir, required_exts=extensions).load_data()
+    shutil.rmtree(results_dir, ignore_errors=True)
 
-    query_nodes = setup_index(documents, data_dir)
+    documents = []
+    for result in results:
+        # code = "\n\n".join([
+        #     block['code'] for idx, block in enumerate(result["blocks"])
+        #     if block["type"] != 'text'
+        # ]).strip()
+        # description = next(
+        #     (block['code']
+        #      for block in result["blocks"] if block["type"] == 'text'),
+        #     ""
+        # ).strip()
+
+        # # data_file = result['data_file']
+        # file_rel_path = result['code_file'].replace(rag_dir, "").lstrip("/")
+        # code = f"File: {file_rel_path}\nDescription: {
+        #     description}\nCode:\n{code}"
+        # doc = Document(text=code, metadata={
+        #     "file_name": file_rel_path,
+        # })
+        # documents.append(doc)
+
+        filtered_blocks = [
+            block for idx, block in enumerate(result["blocks"])
+            if block["type"] != 'text'
+        ]
+        # data_file = result['data_file']
+        file_rel_path = result['code_file'].replace(rag_dir, "").lstrip("/")
+        for idx, block in enumerate(filtered_blocks):
+            file = file_rel_path
+            part = idx + 1
+            code = block["code"].strip()
+            code = f"File: {file}\nPart: {part}\n{code}"
+            doc = Document(text=code, metadata={
+                "file_name": file,
+                "part": part
+            })
+            documents.append(doc)
+
+    # documents = SimpleDirectoryReader(
+    #     rag_dir, required_exts=rag_extensions, recursive=True).load_data()
+
+    chunk_size = 512
+    chunk_overlap = 64
+    top_k = 50
+
+    query_nodes = setup_index(
+        documents, chunk_size=chunk_size, chunk_overlap=chunk_overlap)
 
     # logger.newline()
     # logger.info("RECIPROCAL_RANK: query...")
@@ -72,13 +137,32 @@ if __name__ == "__main__":
     logger.newline()
     logger.info("RELATIVE_SCORE: sample query...")
     result = query_nodes(
-        sample_query, FUSION_MODES.RELATIVE_SCORE)
+        sample_query, FUSION_MODES.RELATIVE_SCORE, threshold=score_threshold, top_k=top_k)
     logger.info(f"RETRIEVED NODES ({len(result["nodes"])})")
     display_source_nodes(sample_query, result["nodes"])
 
-    response = query_llm(sample_query, result['texts'])
-    # logger.info("QUERY RESPONSE:")
-    # logger.success(response)
+    result_texts = [node.text for node in result["nodes"]]
+    token_count = get_token_counts(result_texts, model)
+
+    logger.info("Token counts")
+    logger.log("Max:", token_count['max'], colors=["DEBUG", "SUCCESS"])
+    logger.log("Min:", token_count['min'], colors=["DEBUG", "SUCCESS"])
+    logger.log("Total:", token_count['total'], colors=["DEBUG", "SUCCESS"])
+
+    instructions = "Generate complete working code and a main function. Respond only with the generated code as a single large code block."
+    # contexts = [f"File: {node.metadata['file']}\nPart: {
+    #     node.metadata['part']}\n{node.text}" for node in result["nodes"]]
+    contexts = [
+        f"File: {node.metadata['file_name']}\n{node.text}"
+        for node in result["nodes"]
+    ]
+    response = query_llm(
+        sample_query + "\n" + instructions,
+        contexts,
+        model=model,
+        system=system,
+        max_tokens=max_tokens,
+    )
 
     # Run app
     while True:
@@ -89,13 +173,32 @@ if __name__ == "__main__":
                 print("Exiting query loop.")
                 break
 
-            result = query_nodes(query, FUSION_MODES.RELATIVE_SCORE)
+            result = query_nodes(
+                query, FUSION_MODES.RELATIVE_SCORE, threshold=score_threshold, top_k=top_k)
             logger.info(f"RETRIEVED NODES ({len(result["nodes"])})")
             display_source_nodes(query, result["nodes"])
 
-            response = query_llm(query, result["texts"])
-            # logger.info("QUERY RESPONSE:")
-            # logger.success(response)
+            result_texts = [node.text for node in result["nodes"]]
+            token_count = get_token_counts(result_texts, model)
+
+            logger.info("Token counts")
+            logger.log("Max:", token_count['max'], colors=["DEBUG", "SUCCESS"])
+            logger.log("Min:", token_count['min'], colors=["DEBUG", "SUCCESS"])
+            logger.log("Total:", token_count['total'], colors=[
+                       "DEBUG", "SUCCESS"])
+
+            # contexts = [f"File: {node.metadata['file']}\nPart: {
+            #     node.metadata['part']}\n{node.text}" for node in result["nodes"]]
+            contexts = [
+                f"File: {node.metadata['file_name']}\n{node.text}"
+                for node in result["nodes"]
+            ]
+            response = query_llm(
+                query + "\n" + instructions,
+                contexts,
+                model=model,
+                max_tokens=max_tokens,
+            )
 
         except KeyboardInterrupt:
             print("\nExiting query loop.")
--- llm/token/run_token_counter.py
+++ llm/token/run_token_counter.py
@@ -1,16 +1,85 @@
-from jet.llm.token import token_counter
-from jet.llm.llm_types import Message
-from jet.llm.ollama import OLLAMA_HF_MODELS, count_tokens
+import json
+from jet.llm.token import token_counter, filter_texts
+from jet.logger import logger
+from llama_index.core.base.llms.types import ChatMessage, MessageRole
 
-texts = [
+sample_texts: list[str] = [
     "This is the first example sentence.",
     "Here is another text to test the function.",
     "Let's see how the token count works with multiple items in the batch.",
     "This is the last sentence in the example batch."
 ]
+sample_text = "\n".join(sample_texts)
+sample_chat_messages: list[ChatMessage] = [
+    ChatMessage(
+        role=MessageRole.SYSTEM if i % 2 == 0 else MessageRole.USER,
+        content=text
+    )
+    for i, text in enumerate(sample_texts)
+]
 
-# Model key from OLLAMA_HF_MODELS
-model = "llama3.1"
-# model = "mistral"
-num_tokens = token_counter(texts, model=model)
-print(f"Token count: {num_tokens}")
+if __name__ == "__main__":
+    models = ["llama3.1"]
+    ollama_models = {}
+
+    logger.info("Texts:")
+    logger.debug(json.dumps(sample_texts, indent=2))
+
+    logger.info("Count tokens for: str")
+    for model_name in models:
+        count = token_counter(sample_text, model_name)
+        logger.log("Tokens:", count, colors=["DEBUG", "SUCCESS"])
+
+    logger.info("Count batch tokens for: list[str]")
+    for model_name in models:
+        count = token_counter(sample_texts, model_name)
+        logger.log("Tokens:", count, colors=["DEBUG", "SUCCESS"])
+
+        counts = token_counter(sample_texts, model_name, prevent_total=True)
+        logger.log("List of Tokens:", counts, colors=["DEBUG", "SUCCESS"])
+
+    logger.info("Filter text count")
+    for model_name in models:
+        # Whole numbers
+        max_tokens = 20
+        filtered_text = filter_texts(
+            sample_text, model_name, max_tokens=max_tokens)
+        count = token_counter(filtered_text, model_name)
+        logger.log("Max Tokens:", max_tokens, colors=["GRAY", "DEBUG"])
+        logger.log("New Tokens:", count, colors=["DEBUG", "SUCCESS"])
+        logger.log("Filtered Text:", filtered_text,
+                   colors=["DEBUG", "SUCCESS"])
+
+    logger.info("Filter batch of texts for: list[str]")
+    for model_name in models:
+        # Whole numbers
+        max_tokens = 20
+        filtered_texts = filter_texts(
+            sample_texts, model_name, max_tokens=max_tokens)
+        count = token_counter(filtered_texts, model_name)
+        logger.log("Max Tokens:", max_tokens, colors=["GRAY", "DEBUG"])
+        logger.log("New Tokens:", count, colors=["DEBUG", "SUCCESS"])
+        logger.log("Filtered Texts:", filtered_texts,
+                   colors=["DEBUG", "SUCCESS"])
+
+        # Percentage
+        max_tokens = 0.5
+        filtered_texts = filter_texts(
+            sample_texts, model_name, max_tokens=max_tokens)
+        count = token_counter(filtered_texts, model_name)
+        logger.log("Max Tokens:", max_tokens, colors=["GRAY", "DEBUG"])
+        logger.log("New Tokens:", count, colors=["DEBUG", "SUCCESS"])
+        logger.log("Filtered Texts:", filtered_texts,
+                   colors=["DEBUG", "SUCCESS"])
+
+    logger.info("Filter batch of texts for: list[ChatMessage]")
+    for model_name in models:
+        # Whole numbers
+        max_tokens = 20
+        filtered_texts = filter_texts(
+            sample_chat_messages, model_name, max_tokens=max_tokens)
+        count = token_counter(filtered_texts, model_name)
+        logger.log("Max Tokens:", max_tokens, colors=["GRAY", "DEBUG"])
+        logger.log("New Tokens:", count, colors=["DEBUG", "SUCCESS"])
+        logger.log("Filtered Texts:", filtered_texts,
+                   colors=["DEBUG", "SUCCESS"])
