5f628ac9e39a2472e7466b81701990f208aed61b
Jethro Estrada <jethroestrada237@gmail.com>
2025-01-15 12:14:20 +0800

    Updates

---
+++ llm/semantic_search/llama_index/run_recursive_retriever_search.py
@@ -0,0 +1,96 @@
+import os
+from typing import Generator
+from jet.llm.query.retrievers import setup_recursive_query
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, PromptTemplate
+from llama_index.core.query_engine import RetrieverQueryEngine
+from llama_index.core.base.base_retriever import BaseRetriever
+from llama_index.core.retrievers import QueryFusionRetriever
+from llama_index.core.retrievers.fusion_retriever import FUSION_MODES
+from llama_index.core.schema import Document, NodeWithScore, BaseNode, TextNode, ImageNode
+from jet.llm.utils import display_jet_source_nodes
+from jet.logger import logger
+from jet.llm import call_ollama_chat
+from jet.llm.llm_types import OllamaChatOptions
+from jet.llm.query import setup_index, query_llm
+from jet.llm.ollama import initialize_ollama_settings, large_llm_model
+initialize_ollama_settings()
+
+if __name__ == "__main__":
+    system = (
+        "You are a job applicant providing tailored responses during an interview.\n"
+        "Always answer questions using the provided context as if it is your resume, "
+        "and avoid referencing the context directly.\n"
+        "Some rules to follow:\n"
+        "1. Never directly mention the context or say 'According to my resume' or similar phrases.\n"
+        "2. Provide responses as if you are the individual described in the context, focusing on professionalism and relevance."
+    )
+
+    prompt_template = PromptTemplate(
+        """\
+    Resume details are below.
+    ---------------------
+    {context_str}
+    ---------------------
+    Given the resume details and not prior knowledge, respond to the question.
+    Question: {query_str}
+    Response: \
+    """
+    )
+
+    data_dir = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/data/jet-resume/data"
+    output_dir = "generated/run_recursive_retriever_search"
+    extensions = [".md"]
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    sample_query = "Tell me about yourself."
+
+    documents = SimpleDirectoryReader(
+        data_dir, required_exts=extensions).load_data()
+
+    query_nodes = setup_recursive_query(
+        data_dir, use_cache=False, output_dir=output_dir)
+
+    # logger.newline()
+    # logger.info("RECIPROCAL_RANK: query...")
+    # response = query_nodes(sample_query, FUSION_MODES.RECIPROCAL_RANK)
+
+    # logger.newline()
+    # logger.info("DIST_BASED_SCORE: query...")
+    # response = query_nodes(sample_query, FUSION_MODES.DIST_BASED_SCORE)
+
+    logger.newline()
+    logger.info("RELATIVE_SCORE: sample query...")
+    result = query_nodes(sample_query)
+    logger.info(f"RETRIEVED NODES ({len(result["nodes"])})")
+    display_jet_source_nodes(sample_query, result["nodes"])
+
+    response = query_llm(sample_query, result['texts'])
+    # logger.info("QUERY RESPONSE:")
+    # logger.success(response)
+
+    # Run app
+    while True:
+        # Continuously ask user for queries
+        try:
+            query = input("Enter your query (type 'exit' to quit): ").strip()
+            if query.lower() == "exit":
+                print("Exiting query loop.")
+                break
+
+            result = query_nodes(query)
+            logger.info(f"RETRIEVED NODES ({len(result["nodes"])})")
+            display_jet_source_nodes(query, result["nodes"])
+
+            response = query_llm(query, result["texts"])
+            # logger.info("QUERY RESPONSE:")
+            # logger.success(response)
+
+        except KeyboardInterrupt:
+            print("\nExiting query loop.")
+            break
+        except Exception as e:
+            logger.error(f"Error while processing query: {e}")
+
+    logger.info("\n\n[DONE]", bright=True)
