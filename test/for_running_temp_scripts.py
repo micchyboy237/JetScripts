cf730b50be78051d79c883fadbf4228912f9f1c0
Jethro Estrada <jethroestrada237@gmail.com>
2025-01-18 01:54:03 +0800

    Updates

--- test/for_running_temp_scripts.py
+++ test/for_running_temp_scripts.py
@@ -3,47 +3,10 @@ import inspect
 
 from jet.logger import logger
 from jet.transformers.formatters import format_json
+from jet.utils.class_utils import get_internal_attributes, get_non_empty_attributes
 
 # Assuming last_matching_frame is a frame from the inspect stack
 last_matching_frame = inspect.stack()[-1]
-
-
-def get_non_empty_attributes(obj: Any) -> Dict[str, Any]:
-    """
-    Extracts the non-empty attributes of an object (excluding those starting with "_")
-    and returns them in a dictionary, filtering out attributes with values that are
-    considered empty or falsy.
-
-    Args:
-        obj: The object from which to extract attributes.
-
-    Returns:
-        A dictionary with attribute names as keys and their corresponding
-        non-falsy values as values.
-    """
-    # Filter out attributes that are falsy (None, False, 0, "", [], {}, set()) and those starting with "_"
-    return {
-        attr: getattr(obj, attr)
-        for attr in dir(obj)
-        if not attr.startswith('_') and getattr(obj, attr) not in [None, False, 0, "", [], {}, set()]
-    }
-
-
-def get_internal_attributes(obj: Any) -> Dict[str, Any]:
-    """
-    Extracts the attributes of an object that start with "_" and returns them in a dictionary.
-
-    Args:
-        obj: The object from which to extract attributes.
-
-    Returns:
-        A dictionary with attribute names starting with "_" as keys and their corresponding values.
-    """
-    return {
-        attr: getattr(obj, attr)
-        for attr in dir(obj)
-        if attr.startswith('_')
-    }
 
 
 # Usage example:
