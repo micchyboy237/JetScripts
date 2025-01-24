de00eef9b6aad5dc6b5d1794bdfbd18d6d1cc6ba
Jethro Estrada <jethroestrada237@gmail.com>
2025-01-25 03:04:55 +0800

    Updates

--- mitm-interceptors/open_webui_proxy.py
+++ mitm-interceptors/open_webui_proxy.py
@@ -1,34 +1,9 @@
-from jet.logger import logger
-from jet.transformers.formatters import format_json
 from mitmproxy import http
 
 
 def request(flow: http.HTTPFlow):
-    logger.newline()
-    logger.log("flow.request.path:", flow.request.path,
-               colors=["WHITE", "INFO"])
-    logger.log("flow.request.pretty_host:", flow.request.pretty_host,
-               colors=["WHITE", "INFO"])
-    logger.info("REQUEST:")
-    logger.debug(format_json(flow.request))
-
     if flow.request.path.startswith("/static"):
-        # flow.response = http.Response.make(
-        #     200,  # (optional) status code
-        #     b"Hello World",  # (optional) content
-        #     {"Content-Type": "text/html"},  # (optional) headers
-        # )
         flow.request.port = 8081
-    logger.log("flow.request.port:", flow.request.port,
-               colors=["WHITE", "INFO"])
-    logger.log("flow.request.url:", flow.request.url,
-               colors=["WHITE", "INFO"])
-
-
-def response(flow: http.HTTPFlow):
-    logger.newline()
-    logger.info("RESPONSE:")
-    logger.debug(format_json(flow.response))
 
 
 # Commands
