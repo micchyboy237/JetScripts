ac3a174905abdda778ccc6814819a27c0c9d4ceb
Jethro Estrada <jethroestrada237@gmail.com>
2025-01-25 02:35:25 +0800

    Updates

--- mitm-interceptors/run_proxy_sample.sh
+++ mitm-interceptors/run_proxy_sample.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Default client configuration
-CLIENT_LISTEN_PORT="11434"
-CLIENT_TARGET_URL="http://jetairm1:11434"
-CLIENT_PROXY_SCRIPT="ollama_proxy.py"
+# Default configuration
+LISTEN_PORT="8080"
+TARGET_URL="http://localhost:8085"
 
-# Run mitmdump in client mode
-echo "Running in client mode..."
-mitmdump -s $CLIENT_PROXY_SCRIPT --mode reverse:$CLIENT_TARGET_URL -p $CLIENT_LISTEN_PORT
+echo "Running reverse proxy..."
+mitmdump --mode reverse:$TARGET_URL -p $LISTEN_PORT
 
 # Sample Command
-# Run client
 # ./run_proxy_sample.sh
