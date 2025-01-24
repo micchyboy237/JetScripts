4f82f4704b915086f992ee563dc19523da58ea2d
Jethro Estrada <jethroestrada237@gmail.com>
2025-01-25 03:09:08 +0800

    Updates

---
+++ mitm-interceptors/start_open_webui_static_reverse_proxy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Default configuration
+LISTEN_PORT="8080"
+TARGET_URL="http://jetairm1:8081"
+PROXY_SCRIPT="open_webui_proxy.py"
+
+
+echo "Running reverse proxy with CORS enabled..."
+
+# Run mitmdump with the CORS injection script
+mitmdump --mode reverse:$TARGET_URL -p $LISTEN_PORT -s $PROXY_SCRIPT
+
+# Sample Command
+# ./run_proxy_sample.sh
