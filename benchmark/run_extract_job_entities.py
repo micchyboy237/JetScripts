d69e66e5e7488fed311f48078c0f793367b8cc1e
Jethro Estrada <jethroestrada237@gmail.com>
2025-02-28 03:25:38 +0800

    Updates

--- benchmark/run_extract_job_entities.py
+++ benchmark/run_extract_job_entities.py
@@ -1,5 +1,5 @@
 import json
-from jet.libs.txtai.pipeline.segmentation import Segmentation
+from jet.libs.txtai.pipeline.lemmatizer import lemmatize_text
 from jet.logger import logger
 from jet.memory.httpx import HttpxClient
 from jet.scrapers.utils import clean_text
@@ -8,6 +8,10 @@ from pydantic import BaseModel
 from typing import List
 from shared.data_types.job import JobEntity
 from jet.transformers import format_json
+from llama_index.core.utils import set_global_tokenizer
+from shared.data_types.job import JobData
+from tqdm import tqdm
+from jet.file.utils import save_file, load_file
 
 NER_API_BASE_URL = "http://0.0.0.0:8002/api/v1/ner"
 
@@ -70,38 +74,56 @@ def extract_entities(body: dict):
 
 
 def main():
+    # Running entity extraction for all jobs
+    data_file = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/my-jobs/saved/jobs.json"
+    data: list[JobData] = load_file(data_file) or []
+
     labels = ["technology stack"]
-    chunk_size = 1024
-    chunk_overlap = 128
-    sample_text = "Job Title:\n\nFull Stack Web Developer – Data & Analytics Platform (TikTok Shop Integration)\n\n\n\nJob Description:\n\nWe are looking for a skilled Full Stack Web Developer to help us replicate and build a website similar to \nUpgrade to see actual info\n. The ideal candidate should have experience in building data-driven websites, API integration, and user-friendly dashboards.\n\n\n\nThis role requires someone who can work independently and build a functional, scalable platform that collects and presents analytics, particularly related to TikTok Shop performance data.\n\n\n\nResponsibilities:\n\nDevelop a fully functional web platform similar to \nUpgrade to see actual info\n\nFrontend Development: Design & develop a clean, modern UI/UX using React, Vue, or Angular\n\nBackend Development: Build a scalable system using Node.js, Python (Django/Flask), or PHP (Laravel)\n\nDatabase Management: Set up and manage a database (MySQL, PostgreSQL, or MongoDB) for storing user and analytics data\n\nAPI Integration: Connect and integrate with TikTok Shop APIs & third-party analytics APIs\n\nUser Dashboard & Reports: Create a user-friendly dashboard displaying data insights\n\nPerformance Optimization: Ensure website is fast, secure, and scalable\n\n\n\nRequirements:\n\n3+ years of experience in full-stack web development\n\nProficiency in React, Vue, or Angular (Frontend)\n\nStrong backend skills in Node.js, Python, or PHP (Laravel)\n\nExperience with API integration (TikTok, eCommerce, or Analytics APIs preferred)\n\nDatabase management with MySQL, PostgreSQL, or MongoDB\n\nFamiliarity with web scraping (if needed for analytics data collection)\n\nAbility to create visually appealing dashboards & reports\n\nExperience working with analytics tools & data visualization\n\nStrong problem-solving skills & ability to work independently\n\n\n\nBonus Skills (Not Required but Preferred):\n\nExperience with AI/ML for data analytics\n\nKnowledge of Cloud Hosting (AWS, DigitalOcean, or Firebase)\n\nBackground in eCommerce analytics or TikTok Shop data\n\n\n\nMESSAGE ME IF YOU HAVE ANY QUESTIONS OR COMMENTS!"
-    segmenter = Segmentation(paragraphs=True)
+    chunk_size = 512
 
-    tokenizer =
+    for item in tqdm(data):
+        text = f"{item["title"]}\n\n{item["details"]}"
 
-    splitter = SentenceSplitter(
-        chunk_size=chunk_size, chunk_overlap=chunk_overlap, tokenizer=tokenizer)
-    cleaned_text = clean_text(sample_text)
-    cleaned_text_chunks = splitter.split_text(cleaned_text)
+        single_request_body = {
+            "labels": labels,
+            "chunk_size": chunk_size,
+            "text": text,
+        }
+        entity_result = extract_entity(single_request_body)
+        item['entities']['technology_stack'] = entity_result['technology_stack']
 
-    # Calling extract_entity
-    single_request_body = {
-        "labels": labels,
-        "chunk_size": chunk_size,
-        "text": cleaned_text,
-    }
-    entity_result = extract_entity(single_request_body)
-    logger.debug(f"Entity:")
-    logger.success(format_json(entity_result))
+        save_file(data, data_file)
 
-    # Calling extract_entities
-    multi_request_body = {
-        "labels": labels,
-        "chunk_size": chunk_size,
-        "data": [{"text": text} for text in cleaned_text_chunks],
-    }
-    entities_result = extract_entities(multi_request_body)
-    logger.debug(f"Batch Entities ({len(entities_result)}):")
-    logger.success(format_json(entities_result))
+    # # Calling extract_entity
+    # labels = ["technology stack"]
+    # chunk_size = 250
+    # sample_text = "Job Title:\n\n???? Full Stack Web Developer – Data & Analytics Platform (TikTok Shop Integration)\n\n\n\nJob Description:\n\nWe are looking for a skilled Full Stack Web Developer to help us replicate and build a website similar to \nUpgrade to see actual info\n. The ideal candidate should have experience in building data-driven websites, API integration, and user-friendly dashboards.\n\n\n\nThis role requires someone who can work independently and build a functional, scalable platform that collects and presents analytics, particularly related to TikTok Shop performance data.\n\n\n\nResponsibilities:\n\n? Develop a fully functional web platform similar to \nUpgrade to see actual info\n\n? Frontend Development: Design & develop a clean, modern UI/UX using React, Vue, or Angular\n\n? Backend Development: Build a scalable system using Node.js, Python (Django/Flask), or PHP (Laravel)\n\n? Database Management: Set up and manage a database (MySQL, PostgreSQL, or MongoDB) for storing user and analytics data\n\n? API Integration: Connect and integrate with TikTok Shop APIs & third-party analytics APIs\n\n? User Dashboard & Reports: Create a user-friendly dashboard displaying data insights\n\n? Performance Optimization: Ensure website is fast, secure, and scalable\n\n\n\nRequirements:\n\n???? 3+ years of experience in full-stack web development\n\n???? Proficiency in React, Vue, or Angular (Frontend)\n\n???? Strong backend skills in Node.js, Python, or PHP (Laravel)\n\n???? Experience with API integration (TikTok, eCommerce, or Analytics APIs preferred)\n\n???? Database management with MySQL, PostgreSQL, or MongoDB\n\n???? Familiarity with web scraping (if needed for analytics data collection)\n\n???? Ability to create visually appealing dashboards & reports\n\n???? Experience working with analytics tools & data visualization\n\n???? Strong problem-solving skills & ability to work independently\n\n\n\nBonus Skills (Not Required but Preferred):\n\n? Experience with AI/ML for data analytics\n\n? Knowledge of Cloud Hosting (AWS, DigitalOcean, or Firebase)\n\n? Background in eCommerce analytics or TikTok Shop data\n\n\n\nMESSAGE ME IF YOU HAVE ANY QUESTIONS OR COMMENTS!"
+    # cleaned_text = clean_text(sample_text)
+    # single_request_body = {
+    #     "labels": labels,
+    #     "chunk_size": chunk_size,
+    #     "text": cleaned_text,
+    # }
+    # entity_result = extract_entity(single_request_body)
+    # logger.debug(f"Entity:")
+    # logger.success(format_json(entity_result))
+
+    # # Calling extract_entities
+    # labels = ["technology stack"]
+    # chunk_size = 512
+    # chunk_overlap = 128
+    # set_global_tokenizer(lemmatize_text)
+    # splitter = SentenceSplitter(
+    #     chunk_size=chunk_size, chunk_overlap=chunk_overlap, tokenizer=lemmatize_text)
+    # cleaned_text_chunks = splitter.split_text(cleaned_text)
+    # multi_request_body = {
+    #     "labels": labels,
+    #     "chunk_size": chunk_size,
+    #     "data": [{"text": text} for text in cleaned_text_chunks],
+    # }
+    # entities_result = extract_entities(multi_request_body)
+    # logger.debug(f"Batch Entities ({len(entities_result)}):")
+    # logger.success(format_json(entities_result))
 
 
 if __name__ == "__main__":
--- test/for_running_temp_scripts.py
+++ test/for_running_temp_scripts.py
@@ -1,15 +1,19 @@
 from jet.file.utils import save_file, load_file
 from jet.logger import logger
 from shared.data_types.job import JobData
+from jet.scrapers.utils import clean_text
+from tqdm import tqdm
+
 data_file = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/my-jobs/saved/jobs.json"
 data: list[JobData] = load_file(data_file) or []
 
-filtered_data = [
-    item for item in data
-    if "React Native" in item["entities"]["technology_stack"]
-]
+for item in tqdm(data):
+    item["title"] = clean_text(item["title"])
+    item["details"] = clean_text(item["details"])
+    item["tags"] = [clean_text(tag) for tag in item["tags"]]
+    item["entities"] = {
+        key: [clean_text(v) for v in value]
+        for key, value in item["entities"].items()
+    }
 
-
-# Print sample output
-logger.log("Filtered jobs with React Native:", len(
-    filtered_data), colors=["GRAY", "SUCCESS"])
+    save_file(data, data_file)
