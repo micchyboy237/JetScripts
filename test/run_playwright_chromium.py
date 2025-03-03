b84c39aa8a7105e665ddd83e14cb995d254da191
Jethro Estrada <jethroestrada237@gmail.com>
2025-03-03 09:32:39 +0800

    Updates

--- test/run_playwright_chromium.py
+++ test/run_playwright_chromium.py
@@ -1,17 +1,11 @@
 import asyncio
 import os
-from typing import TypedDict, List, Optional
-from jet.scrapers.browser.playwright import scrape_async, scrape_sync
+from typing import List, Optional
+from jet.scrapers.browser.playwright import scrape_sync, scrape_async, ScrapeHTMLResult, setup_sync_browser_session
 from jet.logger import logger
 
 GENERATED_DIR = "generated"
 os.makedirs(GENERATED_DIR, exist_ok=True)
-
-
-class PageDimensions(TypedDict):
-    width: int
-    height: int
-    deviceScaleFactor: float
 
 
 def build_linkedin_url(query: str, days: int, page: int) -> str:
@@ -20,22 +14,40 @@ def build_linkedin_url(query: str, days: int, page: int) -> str:
             f"&f_TPR=r{days * 86400}&start={page * 25}")
 
 
-def scrape_linkedin_jobs(query: str, days: int, page: int, async_mode: bool = False, wait_for_css: Optional[List[str]] = None) -> str:
+def scrape_linkedin_jobs(query: str, days: int, page: int, async_mode: bool = False, wait_for_css: Optional[List[str]] = None) -> ScrapeHTMLResult:
     url = build_linkedin_url(query, days, page)
     logger.newline()
     logger.log("URL:", url, colors=["GRAY", "ORANGE"])
-    return asyncio.run(scrape_async(url, wait_for_css)) if async_mode else scrape_sync(url, wait_for_css)
+    return scrape_html(url, wait_for_css=wait_for_css, async_mode=async_mode)
 
 
 if __name__ == "__main__":
     query = "software developer"
     days = 7
     page = 0
-    wait_for_css = [".results-context-header__job-count"]
-    html_result = scrape_linkedin_jobs(
-        query, days, page, async_mode=False, wait_for_css=wait_for_css)
-    print("HTML Content:", html_result[:500])
+    # wait_for_css = [".results-context-header__job-count"]
+    wait_for_css = []
 
-    html_result = scrape_linkedin_jobs(
-        query, days, page, async_mode=True, wait_for_css=wait_for_css)
-    print("HTML Content:", html_result[:500])
+    url = build_linkedin_url(query, days, page)
+
+    # Synchronous Example
+    browser = setup_sync_browser_session()
+    result = scrape_sync(url, wait_for_css=wait_for_css, browser=browser)
+    logger.info("Sync scrape results:")
+    logger.success({
+        "dimensions": result['dimensions'],
+        "screenshot": result['screenshot'],
+        "html": result['html'][:100],
+    })
+
+    # Asynchronous Example
+    async def amain():
+        result = await scrape_async(url, wait_for_css=wait_for_css)
+        return result
+    result = asyncio.run(amain())
+    logger.info("Async scrape results:")
+    logger.success({
+        "dimensions": result['dimensions'],
+        "screenshot": result['screenshot'],
+        "html": result['html'][:100],
+    })
