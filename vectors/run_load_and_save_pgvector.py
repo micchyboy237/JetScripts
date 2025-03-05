9154362ef777656f620b27abadf869c01e3b2976
Jethro Estrada <jethroestrada237@gmail.com>
2025-03-06 00:30:53 +0800

    Updates

--- vectors/run_load_and_save_pgvector.py
+++ vectors/run_load_and_save_pgvector.py
@@ -29,57 +29,8 @@ if __name__ == '__main__':
     model = "mxbai-embed-large"
     # model = "nomic-embed-text"
 
-    data_file = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/my-jobs/saved/jobs.json"
-    jobs: JobData = load_file(data_file)
-
-    texts = []
-
-    json_attributes = [
-        "title",
-        "keywords",
-        "tags",
-        # "entities.role",
-        # "entities.application",
-        # "entities.coding_libraries",
-        # "entities.qualifications",
-        "details",
-    ]
-
-    for item in tqdm(jobs):
-        json_parts_dict = extract_values_by_paths(
-            item, json_attributes, is_flattened=True) if json_attributes else None
-        text_parts = []
-        for key, value in json_parts_dict.items():
-            value_str = str(value)
-            if isinstance(value, list):
-                value_str = ", ".join(value)
-            if value_str.strip():
-                text_parts.append(
-                    f"{key.title().replace('_', ' ')}: {value_str}")
-
-        text_content = "\n".join(text_parts) if text_parts else ""
-        texts.append(text_content)
-
-    token_counts_info = get_token_counts_info(texts, model)
-    copy_to_clipboard(token_counts_info)
-
-    logger.debug(f"Jobs ({len(jobs)})")
-    logger.success(format_json({
-        "largest": token_counts_info["max"]["tokens"],
-        "smallest": token_counts_info["min"]["tokens"],
-        "total": token_counts_info["total"],
-    }))
-
-    # Embed texts
-    embed_results = embed_texts(texts)
-    logger.debug(f"Embed Results: {len(embed_results)}")
-    logger.success(f"Embeddings Dim: {len(embed_results[0])}")
-
-    # Save embeddings
-    vectors_with_ids = {generate_key(
-        text): embed_results[idx] for idx, text in enumerate(texts)}
-
-    save_file(vectors_with_ids, "generated/job-embeddings.json")
+    vectors_with_ids: dict[str, list[float]] = load_file(
+        "generated/job-embeddings.json")
 
     dbname = "jobs_db1"
     tablename = "embeddings"
