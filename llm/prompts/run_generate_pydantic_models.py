e7bc2990794de0a72532189dcb5428a91e0cb637
Jethro Estrada <jethroestrada237@gmail.com>
2025-04-10 19:43:38 +0800

    Updates

--- llm/prompts/run_generate_pydantic_models.py
+++ llm/prompts/run_generate_pydantic_models.py
@@ -2,7 +2,7 @@ import os
 from jet.llm.ollama.base import Ollama
 from jet.logger import logger
 from jet.transformers.formatters import format_json
-from jet.llm.prompt_templates.base import generate_json_schema
+from jet.llm.prompt_templates.base import generate_pydantic_models
 from jet.file.utils import save_file
 
 output_dir = os.path.join(
@@ -13,10 +13,11 @@ A list of answers, each with anime title, document number and release year.
 """.strip()
 query = "Top otome villainess anime 2025"
 
-json_schema_context = f"Field Descriptions:\n{field_descriptions}\n\nQuery:\n{query}"
-generated_json_schema = generate_json_schema(context=json_schema_context)
+pydantic_models_context = f"Field Descriptions:\n{field_descriptions}\n\nQuery:\n{query}"
+generated_pydantic_models = generate_pydantic_models(
+    context=pydantic_models_context)
 
-logger.success(format_json(generated_json_schema))
+logger.success(format_json(generated_pydantic_models))
 
-json_schema_file = f"{output_dir}/generated_json_schema.json"
-save_file(generated_json_schema, json_schema_file)
+pydantic_models_file = f"{output_dir}/generated_pydantic_models.py"
+save_file(generated_pydantic_models, pydantic_models_file)
