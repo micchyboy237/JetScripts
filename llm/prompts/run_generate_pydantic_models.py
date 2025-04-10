e9452ea3abed8e5db2d710581ff279497747da01
Jethro Estrada <jethroestrada237@gmail.com>
2025-04-10 21:13:51 +0800

    Updates

--- llm/prompts/run_generate_pydantic_models.py
+++ llm/prompts/run_generate_pydantic_models.py
@@ -1,4 +1,5 @@
 import os
+import re
 from jet.llm.ollama.base import Ollama
 from jet.logger import logger
 from jet.transformers.formatters import format_json
@@ -13,11 +14,26 @@ A list of answers, each with anime title, document number and release year.
 """.strip()
 query = "Top otome villainess anime 2025"
 
+
+def remove_imports(python_code):
+    # Regular expression to match import lines
+    pattern = r'^(import .*$|from .+ import .*$)'
+
+    # Remove the lines that match the import pattern
+    python_code_without_imports = re.sub(
+        pattern, '', python_code, flags=re.MULTILINE)
+
+    return python_code_without_imports.strip()
+
+
 pydantic_models_context = f"Field Descriptions:\n{field_descriptions}\n\nQuery:\n{query}"
-generated_pydantic_models = generate_pydantic_models(
+generated_python_code = generate_pydantic_models(
     context=pydantic_models_context)
 
-logger.success(generated_pydantic_models)
+schema_str = remove_imports(generated_python_code)
 
-pydantic_models_file = f"{output_dir}/generated_pydantic_models.py"
-save_file(generated_pydantic_models, pydantic_models_file)
+
+output_file = f"{output_dir}/generated_pydantic_models.py"
+save_file(generated_python_code, output_file)
+output_file = f"{output_dir}/generated_pydantic_schema.txt"
+save_file(schema_str, output_file)
