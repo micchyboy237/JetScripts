8d37c058313626b3a3989e2e1cdded16e83af961
Jethro Estrada <jethroestrada237@gmail.com>
2025-04-27 00:50:28 +0800

    Update

---
+++ test/hf_text_generation/mlx/run_gemma3_1B_it_qat_4bit.py
@@ -0,0 +1,48 @@
+from mlx_lm import load, generate, stream_generate
+
+# Load the model and tokenizer
+model, tokenizer = load("mlx-community/gemma-3-1b-it-qat-4bit")
+
+prompt = """
+```python
+def calculate_area_of_circle(radius):
+    \"\"\"Calculate the area of a circle given its radius.\"\"\"
+"""
+max_tokens = -1
+
+
+# Apply chat template if available (for instruction-tuned models)
+# if tokenizer.chat_template is not None:
+#     messages = [
+#         {"role": "system", "content": system_prompt},
+#         {"role": "user", "content": prompt},
+#     ]
+#     prompt = tokenizer.apply_chat_template(
+#         messages, add_generation_prompt=True)
+
+
+# Generate the response
+print("\nGenerate...")
+response = generate(
+    model,
+    tokenizer,
+    prompt=prompt,
+    max_tokens=max_tokens,
+    verbose=True,
+)
+
+# Print the generated response
+print("\nGenerated Response:\n")
+print(response)
+
+# Stream generate the response
+print("\nStream generate...")
+response_stream = stream_generate(
+    model,
+    tokenizer,
+    prompt=prompt,
+    max_tokens=max_tokens
+)
+
+for response in response_stream:
+    print(response.text, end='', flush=True)
