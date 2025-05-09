a6262a2e277b537d9323e0e1ce7c39f35c7952be
Jethro Estrada <jethroestrada237@gmail.com>
2025-05-09 19:30:33 +0800

    Updates

--- llm/mlx/run_mlx_chunker.py
+++ llm/mlx/run_mlx_chunker.py
@@ -5,18 +5,19 @@ from pathlib import Path
 from typing import Generator, TypedDict, Union
 
 from jet.code.utils import ProcessedResult, preprocess_notebooks_to_markdowns
-from jet.features.rag_llm_generation import SimilarityResult, rerank_llm
+from jet.features.rag_llm_generation import SimilarityResult, rerank_llm, rewrite_query
 from jet.file.utils import load_file, save_file
 from jet.llm.mlx.base import MLX
 from jet.llm.mlx.mlx_types import ModelKey
-from jet.llm.mlx.token_utils import chunk_text, merge_texts
+from jet.llm.mlx.token_utils import chunk_text, get_tokenizer_fn, merge_texts
 from jet.llm.mlx.utils import get_model_max_tokens
 from jet.logger import logger
+from jet.scrapers.utils import MergedTextsResult, merge_texts_by_hierarchy
 
-MODEL: ModelKey = "qwen3-0.6b-4bit"
+MODEL: ModelKey = "llama-3.2-3b-instruct-4bit"
 
 SYSTEM_PROMPT = """
-You are a code generation assistant. Given relevant code context, generate a Python class named Chunker that splits input data into logical chunks. Focus on clean, efficient, and well-documented code. Output only the class definition.
+You are a code generation assistant. Given relevant chunking logic in context, generate a Python class named Chunker. Focus on clean, efficient, and well-documented code. Output only the python code block (```python) any other additional text.
 """
 
 
@@ -31,9 +32,7 @@ class GenerationResult(TypedDict):
     response: str
 
 
-def call_llm_generation(texts: list[str]) -> GenerationResult:
-    context = mlx.filter_docs(texts)
-
+def call_llm_generation(context: str) -> GenerationResult:
     response = ""
     for chunk in mlx.stream_chat(
         context,
@@ -50,10 +49,11 @@ def call_llm_generation(texts: list[str]) -> GenerationResult:
     }
 
 
-def preprocess_data(texts: list[str]) -> list[str]:
-    chunk_size = 300
-    chunks = chunk_text(texts, chunk_size)
-    return chunks
+def preprocess_data(texts: list[str], chunk_size: int) -> list[str]:
+    chunks = merge_texts_by_hierarchy(
+        "\n\n".join(texts), get_tokenizer_fn(MODEL), max_tokens=chunk_size)
+    # chunks = chunk_text(texts, chunk_size, overlap=40, model=MODEL)
+    return [chunk["text"] for chunk in chunks]
 
 
 class GeneratorGenerationResult(TypedDict):
@@ -63,26 +63,32 @@ class GeneratorGenerationResult(TypedDict):
     reranked_data: list[SimilarityResult]
 
 
-def run_generate_chunker_class(json_file_or_dir) -> Generator[GeneratorGenerationResult, None, None]:
+def run_generate_chunker_class(query: str, json_file_or_dir: str) -> Generator[GeneratorGenerationResult, None, None]:
+    chunk_size = 300
     # Process the markdown file
     if os.path.isdir(json_file_or_dir):
         for file in Path(json_file_or_dir).glob('*.json'):
             logger.info(f"\nProcessing {file}")
             data: list[ProcessedResult] = load_file(str(file))
             texts = [d["text"] for d in data]
-            reranked_data = rerank_llm(texts)
+            reranked_data = rerank_llm(query, texts)
             reranked_texts = [doc["text"] for doc in reranked_data]
-            preprocessed_data = preprocess_data(reranked_texts)
-            result = call_llm_generation(preprocessed_data)
+            preprocessed_data = preprocess_data(reranked_texts, chunk_size)
+            contexts = mlx.filter_docs(
+                preprocessed_data, chunk_size=chunk_size)
+            context = "\n\n".join(contexts)
+            result = call_llm_generation(context)
             yield {"source": file, "code": result["response"], "data": preprocessed_data, "reranked_data": reranked_data}
     else:
         logger.info(f"\nProcessing {json_file_or_dir}")
-        data: list[ProcessedResult] = load_file(json_file_or_dir)
+        data: list[ProcessedResult] = load_file(str(file))
         texts = [d["text"] for d in data]
-        reranked_data = rerank_llm(texts)
+        reranked_data = rerank_llm(query, texts)
         reranked_texts = [doc["text"] for doc in reranked_data]
-        preprocessed_data = preprocess_data(reranked_texts)
-        result = call_llm_generation(preprocessed_data)
+        preprocessed_data = preprocess_data(reranked_texts, chunk_size)
+        contexts = mlx.filter_docs(preprocessed_data, chunk_size=chunk_size)
+        context = "\n\n".join(contexts)
+        result = call_llm_generation(context)
         yield {"source": json_file_or_dir, "code": result["response"], "data": preprocessed_data, "reranked_data": reranked_data}
 
 
@@ -94,7 +100,10 @@ if __name__ == "__main__":
 
     preprocess_notebooks_to_markdowns(md_dir, output_dir)
 
-    results = run_generate_chunker_class(output_dir)
+    query = "Chunking data"
+    # query = rewrite_query(query)
+
+    results = run_generate_chunker_class(query, output_dir)
 
     chunker_classes_output_dir = os.path.join(
         os.path.dirname(__file__), "generated", os.path.splitext(os.path.basename(__file__))[0], "chunker_classes")
