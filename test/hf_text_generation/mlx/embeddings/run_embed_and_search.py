b04ea0aeca019a5c2961ccd30c55e262370ab21b
Jethro Estrada <jethroestrada237@gmail.com>
2025-05-12 04:11:21 +0800

    Updates

--- test/hf_text_generation/mlx/embeddings/run_embed_and_search.py
+++ test/hf_text_generation/mlx/embeddings/run_embed_and_search.py
@@ -1,13 +1,17 @@
 import json
+from jet.file.utils import save_file
+from jet.llm.mlx.mlx_types import EmbedModelKey
+from jet.llm.mlx.models import AVAILABLE_EMBED_MODELS, resolve_model
 from jet.logger import logger
 import mlx_embeddings
 import mlx.core as mx
 import numpy as np
 
 
-def load_model():
+def load_model(embed_model: EmbedModelKey):
     """Load the pre-trained model and tokenizer."""
-    return mlx_embeddings.load("mlx-community/all-MiniLM-L6-v2-4bit")
+    model = resolve_model(embed_model)
+    return mlx_embeddings.load(model)
 
 
 def generate_embeddings(model, tokenizer, texts):
@@ -44,8 +48,11 @@ def search(query, corpus_texts, model, tokenizer, top_k=3):
 
 
 def main():
-    # Load model and tokenizer
-    model, tokenizer = load_model()
+    import os
+
+    output_dir = os.path.join(
+        os.path.dirname(__file__), "generated", os.path.splitext(os.path.basename(__file__))[0])
+    os.makedirs(output_dir, exist_ok=True)
 
     # Sample corpus
     corpus = [
@@ -60,17 +67,32 @@ def main():
     query = "I like grapes"
     top_k = 5
 
-    # Perform search
-    results = search(query, corpus, model, tokenizer, top_k=top_k)
+    # Iterate through all available models
+    results_dict = {}
+    for model_key in AVAILABLE_EMBED_MODELS.keys():
+        results_dict[model_key] = []
+        results = results_dict[model_key]
 
-    # Print results
-    logger.newline()
-    logger.log(f"Query:", query, colors=["GRAY", "INFO"])
-    logger.gray("Top matching texts:")
-    for i, result in enumerate(results, 1):
-        score = result['score'] * 100
-        logger.log(f"{i}.", f"{score:.2f}%", json.dumps(result['text'])[:100], colors=[
-                   "DEBUG", "SUCCESS", "WHITE"])
+        logger.newline()
+
+        # Load model and tokenizer
+        logger.log(f"Testing model:", model_key, colors=["GRAY", "ORANGE"])
+        model, tokenizer = load_model(model_key)
+
+        # Perform search
+        search_results = search(query, corpus, model, tokenizer, top_k=top_k)
+
+        # Print search results
+        logger.log("Query:", query, colors=["GRAY", "INFO"])
+        logger.gray(f"Top matching texts (model: {model_key}):")
+        for i, result in enumerate(search_results, 1):
+            score = result['score'] * 100
+            logger.log(f"{i}.", f"{score:.2f}%", json.dumps(result['text'])[:100], colors=[
+                       "DEBUG", "SUCCESS", "WHITE"])
+
+            results.append(result)
+            save_file(
+                results_dict, f"{output_dir}/mlx_embed_and_search_results.json")
 
 
 if __name__ == "__main__":
