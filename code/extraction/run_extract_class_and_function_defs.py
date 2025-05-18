dd7394a3b190598abf832da58488aa159222a733
Jethro Estrada <jethroestrada237@gmail.com>
2025-05-18 18:43:27 +0800

    Updates

--- code/extraction/run_extract_class_and_function_defs.py
+++ code/extraction/run_extract_class_and_function_defs.py
@@ -7,6 +7,14 @@ from jet.transformers.formatters import format_json
 if __name__ == "__main__":
     result = extract_class_and_function_defs(
         "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/converted_doc_scripts/all-rag-techniques")
+
+    code = ""
+    for filepath, defs in result.items():
+        code += f"# {filepath}\n"
+        for item in defs:
+            code += item + "\n"
+        code += "\n"  # Add an extra newline after each file block
+
     logger.gray("Result:")
     logger.success(format_json(result))
-    copy_to_clipboard(result)
+    copy_to_clipboard(code)
