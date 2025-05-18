d43a8cd75ff6aafc4f308c6a6c26acc7c27b257e
Jethro Estrada <jethroestrada237@gmail.com>
2025-05-18 19:18:37 +0800

    Updates

---
+++ executor/run_python_files_in_directory.py
@@ -0,0 +1,5 @@
+from jet.executor.python_runner import run_python_files_in_directory
+
+if __name__ == "__main__":
+    run_python_files_in_directory(
+        "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/converted_doc_scripts/all-rag-techniques")
