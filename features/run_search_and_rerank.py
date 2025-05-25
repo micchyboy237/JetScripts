975116b096a9af862718f2d4d07c9a0fb89d840c
Jethro Estrada <jethroestrada237@gmail.com>
2025-05-25 14:19:01 +0800

    Updates

--- features/run_search_and_rerank.py
+++ features/run_search_and_rerank.py
@@ -124,8 +124,8 @@ if __name__ == "__main__":
     shutil.rmtree(output_dir, ignore_errors=True)
     os.makedirs(output_dir, exist_ok=True)
 
-    query = f"List trending isekai reincarnation anime this year."
-    # query = "Tips and links to 2025 online registration steps for TikTok live selling in the Philippines."
+    # query = f"List trending isekai reincarnation anime this year."
+    query = "Tips and links to 2025 online registration steps for TikTok live selling in the Philippines."
     top_k = 10
 
     model_path = "mlx-community/Llama-3.2-3B-Instruct-4bit"
