f718424644b8d0c549d085c6483866f1241877b2
Jethro Estrada <jethroestrada237@gmail.com>
2025-06-05 16:02:50 +0800

    Updates

--- models/embeddings/run_get_embedding_function.py
+++ models/embeddings/run_get_embedding_function.py
@@ -8,6 +8,13 @@ if __name__ == "__main__":
     result = bert_tokenize("I can feel the magic, can you?")
     print(f"BERT Tokens: {result}")
 
+    # Span marker example
+    span_marker_tokenize = get_embedding_function(
+        "tomaarsen/span-marker-bert-base-fewnerd-fine-super")
+    result = span_marker_tokenize(
+        "Cleopatra VII, also known as Cleopatra the Great, was the last active ruler of the Ptolemaic Kingdom of Egypt. She was born in 69 BCE and ruled Egypt from 51 BCE until her death in 30 BCE.")
+    print(f"Span Marker Tokens: {result}")
+
     # Sentence Transformer example
     st_tokenize = get_embedding_function(
         "sentence-transformers/all-MiniLM-L6-v2")
