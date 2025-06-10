729192fe3668f1baff6e5e0f69345ff613ae3cf7
Jethro Estrada <jethroestrada237@gmail.com>
2025-06-11 00:10:50 +0800

    Updates

--- models/tasks/run_hybrid_search_docs.py
+++ models/tasks/run_hybrid_search_docs.py
@@ -7,8 +7,7 @@ from jet.models.tasks.hybrid_search_docs import search_docs
 if __name__ == "__main__":
     docs_file = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/features/generated/run_search_and_rerank/docs.json"
     query = "List all ongoing and upcoming isekai anime 2025."
-    results = search_docs(
-        docs_file, query, rerank_top_k=10, embedder_model="all-MiniLM-L12-v2")
+    results = search_docs(docs_file, query, rerank_top_k=10)
     for result in results:
         logger.success(
             f"\nRank {result['rank']} (Document ID {result['doc_id']}):")
