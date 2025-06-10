d3ea8f3653224c601189213099313584734c4f8f
Jethro Estrada <jethroestrada237@gmail.com>
2025-06-10 21:20:12 +0800

    Updates

--- converted_doc_scripts/rag-cookbooks/advanced_rag_techniques/contextual_rag.py
+++ converted_doc_scripts/rag-cookbooks/advanced_rag_techniques/contextual_rag.py
@@ -1,8 +1,8 @@
-from athina.evals import RagasContextRelevancy
-from athina.keys import AthinaApiKey, OpenAiApiKey
-from athina.loaders import Loader
+# from athina.evals import RagasContextRelevancy
+# from athina.keys import AthinaApiKey, OpenAiApiKey
+# from athina.loaders import Loader
 from datasets import Dataset
-from google.colab import userdata
+# from google.colab import userdata
 from jet.llm.ollama.base_langchain import ChatOllama
 from jet.llm.ollama.base_langchain import OllamaEmbeddings
 from jet.logger import CustomLogger
@@ -18,7 +18,8 @@ import os
 import pandas as pd
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-log_file = os.path.join(script_dir, f"{os.path.splitext(os.path.basename(__file__))[0]}.log")
+log_file = os.path.join(
+    script_dir, f"{os.path.splitext(os.path.basename(__file__))[0]}.log")
 logger = CustomLogger(log_file, overwrite=True)
 logger.info(f"Logs: {log_file}")
 
@@ -38,7 +39,7 @@ logger.info("# **Contextual RAG**")
 # !pip install --q athina chromadb
 
 # os.environ["OPENAI_API_KEY"] = userdata.get('OPENAI_API_KEY')
-os.environ['ATHINA_API_KEY'] = userdata.get('ATHINA_API_KEY')
+# os.environ['ATHINA_API_KEY'] = userdata.get('ATHINA_API_KEY')
 
 """
 ## **Indexing**
@@ -47,7 +48,8 @@ logger.info("## **Indexing**")
 
 embeddings = OllamaEmbeddings(model="mxbai-embed-large")
 
-loader = CSVLoader("./context.csv")
+loader = CSVLoader(
+    "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/converted_doc_scripts/rag-cookbooks/data/context.csv")
 documents = loader.load()
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
@@ -116,8 +118,9 @@ response = []
 contexts = []
 
 for query in questions:
-  response.append(rag_chain.invoke(query))
-  contexts.append([docs.page_content for docs in compression_retriever.get_relevant_documents(query)])
+    response.append(rag_chain.invoke(query))
+    contexts.append(
+        [docs.page_content for docs in compression_retriever.get_relevant_documents(query)])
 
 data = {
     "query": questions,
@@ -152,6 +155,7 @@ AthinaApiKey.set_key(os.getenv('ATHINA_API_KEY'))
 
 dataset = Loader().load_dict(df_dict)
 
-RagasContextRelevancy(model="llama3.1", request_timeout=300.0, context_window=4096).run_batch(data=dataset).to_df()
+RagasContextRelevancy(model="llama3.1", request_timeout=300.0,
+                      context_window=4096).run_batch(data=dataset).to_df()
 
 logger.info("\n\n[DONE]", bright=True)
