c98173a4905faa19c8e54993fa5017aca9c86314
Jethro Estrada <jethroestrada237@gmail.com>
2025-06-11 08:35:14 +0800

    Updates

--- models/tasks/run_hybrid_search_docs_with_bm25.py
+++ models/tasks/run_hybrid_search_docs_with_bm25.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     # docs_file = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/models/tasks/data/browser_search_texts.json"
     query = "List all ongoing and upcoming isekai anime 2025."
     docs = load_file(docs_file)
-    results = search_docs(query, docs, rerank_top_k=10)
+    results = search_docs(query, docs, top_k=None, rerank_top_k=10)
     for result in results:
         logger.success(
             f"\nRank {result['rank']} (Document Index {result['doc_index']}):")
