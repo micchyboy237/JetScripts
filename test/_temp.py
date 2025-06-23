4257d8a087d3ec2ee313290b200d4da82dfdd65f
Jethro Estrada <jethroestrada237@gmail.com>
2025-06-23 13:57:22 +0800

    Updates

--- test/_temp.py
+++ test/_temp.py
@@ -20,7 +20,21 @@ if __name__ == "__main__":
     logger.gray("analyze_markdown:")
     logger.success(results)
 
-    md_content = load_file(md_file)
+    # md_content = load_file(md_file)
+    md_content = "## Industry\n* [News](https://myanimelist.net/news)\n* [Featured Articles](https://myanimelist.net/featured)"
     results = get_md_header_contents(md_content, ignore_links=True)
-    logger.gray("get_md_header_contents:")
+    results = [{
+        "header": result["header"],
+        "content": result["content"],
+        "text": result["text"],
+    } for result in results]
+    expected = [{
+        "header": "## Industry",
+        "content": "* News\n* Featured Articles",
+        "text": "## Industry\n* News\n* Featured Articles"
+    }]
+    logger.gray("results:")
     logger.success(format_json(results))
+    logger.gray("expected:")
+    logger.success(format_json(expected))
+    assert results == expected
