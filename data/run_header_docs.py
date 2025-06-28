4649bb730809b3b193f450206ecf75d3963273bb
Jethro Estrada <jethroestrada237@gmail.com>
2025-06-29 04:26:07 +0800

    Updates

--- data/run_header_docs.py
+++ data/run_header_docs.py
@@ -1,10 +1,11 @@
 import os
-from jet.code.markdown_utils import get_summary, parse_markdown
+from jet.code.markdown_utils import analyze_markdown, parse_markdown
 from jet.data.header_docs import HeaderDocs
 from jet.file.utils import load_file, save_file
 
 if __name__ == "__main__":
-    html_file = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/features/generated/run_search_and_rerank/top_isekai_anime_2025/pages/animebytes.in_15_best_upcoming_isekai_anime_in_2025/page.html"
+    # html_file = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/features/generated/run_search_and_rerank/top_isekai_anime_2025/pages/animebytes.in_15_best_upcoming_isekai_anime_in_2025/page.html"
+    html_file = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/features/generated/run_search_and_rerank/top_rag_strategies_reddit_2025/pages/www.reddit.com_r_rag_comments_1j4r4wj_10_rag_papers_you_should_read_from_february_2025/page.html"
     output_dir = os.path.join(
         os.path.dirname(__file__), "generated", os.path.splitext(
             os.path.basename(__file__))[0]
@@ -13,8 +14,8 @@ if __name__ == "__main__":
     html = load_file(html_file)
     save_file(html, f"{output_dir}/page.html")
 
-    summary = get_summary(html_file)
-    save_file(summary, f"{output_dir}/summary.json")
+    analysis = analyze_markdown(html_file)
+    save_file(analysis, f"{output_dir}/analysis.json")
 
     tokens = parse_markdown(html_file, merge_contents=True)
     save_file(tokens, f"{output_dir}/markdown_tokens.json")
@@ -24,6 +25,9 @@ if __name__ == "__main__":
 
     all_texts = header_docs.as_texts()
     save_file(all_texts, f"{output_dir}/all_texts.json")
+
+    md_content = '\n\n'.join(all_texts)
+    save_file(md_content, f"{output_dir}/md_content.md")
 
     all_nodes = header_docs.as_nodes()
     save_file(all_nodes, f"{output_dir}/all_nodes.json")
