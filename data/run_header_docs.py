f70d882acb6bbe4538545c275b4baf58dbc3bdb6
Jethro Estrada <jethroestrada237@gmail.com>
2025-07-01 22:53:18 +0800

    Updates

--- data/run_header_docs.py
+++ data/run_header_docs.py
@@ -2,10 +2,12 @@ import os
 import shutil
 from jet.code.markdown_utils import analyze_markdown, parse_markdown
 from jet.data.header_docs import HeaderDocs
-from jet.data.header_utils._split_and_merge_headers import split_and_merge_headers
+from jet.data.header_utils import split_and_merge_headers, prepare_for_rag, search_headers
 from jet.file.utils import load_file, save_file
+from jet.logger import logger
 
 if __name__ == "__main__":
+    docs_file = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/features/generated/run_search_and_rerank/top_isekai_anime_2025/docs.json"
     html_file = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/features/generated/run_search_and_rerank/top_isekai_anime_2025/pages/bakabuzz_com_26_upcoming_isekai_anime_of_2025_you_must_watch/page.html"
     # html_file = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/features/generated/run_search_and_rerank/top_rag_strategies_reddit_2025/pages/www.reddit.com_r_rag_comments_1j4r4wj_10_rag_papers_you_should_read_from_february_2025/page.html"
     output_dir = os.path.join(
@@ -30,10 +32,6 @@ if __name__ == "__main__":
     header_docs = HeaderDocs.from_tokens(tokens)
     save_file(header_docs, f"{output_dir}/header_docs.json")
 
-    chunked_nodes = split_and_merge_headers(
-        header_docs.root, model="all-MiniLM-L6-v2", chunk_size=50, chunk_overlap=10)
-    save_file(chunked_nodes, f"{output_dir}/chunked_nodes.json")
-
     all_texts = header_docs.as_texts()
     save_file(all_texts, f"{output_dir}/all_texts.json")
 
@@ -54,3 +52,21 @@ if __name__ == "__main__":
 
     header_tree = header_docs.as_tree()
     save_file(header_tree, f"{output_dir}/header_tree.json")
+
+    # RAG search
+    logger.info("\nStart RAG search...")
+
+    docs = load_file(docs_file)
+    query = docs["query"]
+    top_k = 5
+    model = "all-MiniLM-L6-v2"
+
+    chunked_nodes = split_and_merge_headers(
+        header_docs.root, model=model, chunk_size=50, chunk_overlap=10)
+    save_file(chunked_nodes, f"{output_dir}/chunked_nodes.json")
+
+    vector_store = prepare_for_rag(chunked_nodes, model=model)
+
+    search_results = search_headers(
+        query, vector_store, model=model, top_k=top_k)
+    save_file(search_results, f"{output_dir}/search_results.json")
