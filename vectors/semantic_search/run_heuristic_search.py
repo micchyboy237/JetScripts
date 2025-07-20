48280a50f945cca3f11a8292d19ea6058bd8d6dd
Jethro Estrada <jethroestrada237@gmail.com>
2025-07-21 00:10:36 +0800

    Updates

--- vectors/semantic_search/run_heuristic_search.py
+++ vectors/semantic_search/run_heuristic_search.py
@@ -7,31 +7,44 @@ from shared.data_types.job import JobData
 
 OUTPUT_DIR = os.path.join(
     os.path.dirname(__file__), "generated", os.path.splitext(os.path.basename(__file__))[0])
-shutil.rmtree(OUTPUT_DIR, ignore_errors=True)
 
-if __name__ == "__main__":
+
+def format_sub_dir(text: str) -> str:
+    return text.lower().strip('.,!?').replace(' ', '_').replace('.', '_').replace(',', '_').replace('!', '_').replace('?', '_').strip()
+
+
+def main(query: str):
     data_file = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/my-jobs/saved/jobs.json"
     data: list[JobData] = load_file(data_file)
     texts = [f"{d["title"]}\n{d["details"]}" for d in data]
 
-    # query = "React Native"
-    query = "React.js web with Python AI development"
+    output_dir = f"{OUTPUT_DIR}/{format_sub_dir(query)}"
+    shutil.rmtree(output_dir, ignore_errors=True)
 
     query_candidates = extract_query_candidates(query)
-    results = get_bm25_similarities(query_candidates, texts)
+    ids = [job["id"] for job in data]
+
+    results = get_bm25_similarities(query_candidates, texts, ids=ids)
 
     save_file({
         "query": query,
         "query_candidates": query_candidates,
         "count": len(results),
         "results": results
-    }, f"{OUTPUT_DIR}/results.json")
+    }, f"{output_dir}/results.json")
 
-    results_old = get_bm25_similarities_old(query_candidates, texts)
+    results_old = get_bm25_similarities_old(query_candidates, texts, ids=ids)
 
     save_file({
         "query": query,
         "query_candidates": query_candidates,
         "count": len(results_old),
         "results": results_old
-    }, f"{OUTPUT_DIR}/results_old.json")
+    }, f"{output_dir}/results_old.json")
+
+
+if __name__ == "__main__":
+    query = "React Native"
+    main(query)
+    query = "React.js web with Python AI development"
+    main(query)
