9d13f7e4683a7d6193e59350fec382d60b24f55b
Jethro Estrada <jethroestrada237@gmail.com>
2025-08-03 13:02:07 +0800

    Updates

--- vectors/clusters/run_cluster_retrieval.py
+++ vectors/clusters/run_cluster_retrieval.py
@@ -1,6 +1,10 @@
 import os
+from typing import List, Tuple
 from jet.file.utils import save_file
-from jet.vectors.clusters.retrieval import LLMGenerator, RetrievalConfig, VectorRetriever
+from jet.llm.llm_generator import LLMGenerator
+from jet.logger import logger
+from jet.logger.config import colorize_log
+from jet.vectors.clusters.retrieval import RetrievalConfigDict, VectorRetriever
 
 OUTPUT_DIR = os.path.join(
     os.path.dirname(__file__), "generated", os.path.splitext(os.path.basename(__file__))[0])
@@ -8,7 +12,15 @@ OUTPUT_DIR = os.path.join(
 
 def main():
     """Runner script for retrieval and response generation."""
-    config = RetrievalConfig()
+    config: RetrievalConfigDict = {
+        "min_cluster_size": 2,
+        "k_clusters": 2,
+        "top_k":  None,
+        "cluster_threshold": 20,
+        "model_name": 'mxbai-embed-large',
+        "cache_file":  None,
+        "threshold": None,
+    }
     corpus = [
         "Machine learning is a method of data analysis that automates model building.",
         "Supervised learning uses labeled data to train models for prediction.",
@@ -29,18 +41,17 @@ def main():
     retriever.build_index()
     top_chunks = retriever.retrieve_chunks(query)
 
-    generator = LLMGenerator()
-    response = generator.generate_response(query, top_chunks)
-
-    print("Query:", query)
-    print("\nTop relevant chunks for RAG:")
+    logger.info("\nTop relevant chunks for RAG:")
     for i, (chunk, score) in enumerate(top_chunks, 1):
-        print(f"{i}. {chunk} (Similarity: {score:.4f})")
-    print("\nGenerated Response:")
-    print(response)
+        print(f"{colorize_log(f"{i}.", "ORANGE")} | {
+              colorize_log(f"{score:.4f}", "SUCCESS")} | {chunk[:50]}")
+
+    generator = LLMGenerator()
+    response = generator.generate_response(
+        query, top_chunks, generation_config={"verbose": True})
 
     save_file(top_chunks, f"{OUTPUT_DIR}/top_chunks.json")
-    save_file(f"# LLM Generation\n\n## Query\n\n{query}\n\n## Response\n\n{response}",
+    save_file(f"# LLM Generation\n\n## Query\n\n{query}\n\n## Context\n\n{response["context"]}\n\n## Response\n\n{response["response"]}",
               f"{OUTPUT_DIR}/llm_generation.md")
 
 
