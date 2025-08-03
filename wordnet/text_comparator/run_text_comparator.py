42c413a645e3fadf907b4aea6823dff35fe93c70
Jethro Estrada <jethroestrada237@gmail.com>
2025-08-03 13:42:30 +0800

    Updates

---
+++ wordnet/text_comparator/run_text_comparator.py
@@ -0,0 +1,8 @@
+from jet.wordnet.text_comparator import display_text_comparison
+
+
+if __name__ == "__main__":
+    # Example usage
+    text1 = "The quick brown fox jumps over the lazy dog."
+    text2 = "A quick brown fox jumped over a lazy dog."
+    display_text_comparison(text1, text2)
