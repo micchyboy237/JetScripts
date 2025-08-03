d0d4d145102398110df6138bb2dc1db3259c46a7
Jethro Estrada <jethroestrada237@gmail.com>
2025-08-04 00:29:13 +0800

    Updates

--- vectors/clusters/run_cluster_retrieval.py
+++ vectors/clusters/run_cluster_retrieval.py
@@ -12,8 +12,8 @@ from jet.logger import logger
 from jet.logger.config import colorize_log
 from jet.models.tokenizer.base import count_tokens, get_tokenizer_fn
 from jet.utils.text import format_sub_dir
-from jet.vectors.clusters.retrieval import ChunkSearchResult, RetrievalConfig, VectorRetriever
-from jet.wordnet.text_chunker import chunk_texts
+from jet.vectors.clusters.retrieval import ChunkSearchResult, RetrievalConfigDict, VectorRetriever
+from jet.wordnet.text_chunker import chunk_texts_with_data
 
 OUTPUT_DIR = os.path.join(
     os.path.dirname(__file__), "generated", os.path.splitext(os.path.basename(__file__))[0])
@@ -145,7 +145,7 @@ def main():
     embed_model: EmbedModelType = "all-MiniLM-L6-v2"
     llm_model: LLMModelType = "qwen3-1.7b-4bit"
 
-    config: RetrievalConfig = {
+    config: RetrievalConfigDict = {
         "embed_model": embed_model,
         "min_cluster_size": 2,
         "k_clusters": 3,
@@ -160,50 +160,76 @@ def main():
 
     query = "Top isekai anime 2025."
     chunked_docs = []
-    texts_with_metadata = []
-    for doc_index, header_doc in enumerate(header_docs):
+    all_chunks_with_metadata = []
+    for header_doc in header_docs:
+        doc_index = header_doc["doc_index"]
         header = header_doc["header"]
         content = header_doc['content']
         source = header_doc.get("source", "Unknown Source")
         parent_header = header_doc.get("parent_header", None)
-        buffer: int = count_tokens(config["embed_model"], header)
-        chunks = chunk_texts(content, chunk_size,
-                             chunk_overlap, config["embed_model"], buffer)
-        chunks_with_headers = [f"{header}\n{chunk}" for chunk in chunks]
+        buffer: int = count_tokens(embed_model, header)
+        doc_ids = [header_doc["id"]]
+        chunks = chunk_texts_with_data(content, chunk_size,
+                                       chunk_overlap, embed_model, doc_ids, buffer)
 
+        chunks_with_metadata = []
         # Create metadata for each chunk
-        for chunk_idx, chunk in enumerate(chunks_with_headers):
-            # Estimate start_idx and end_idx based on chunk position
-            start_idx = chunk_idx * (chunk_size - chunk_overlap)
-            end_idx = start_idx + len(chunk.split())
-            texts_with_metadata.append((chunk, {
-                "source": source,
-                "doc_index": doc_index,
-                "start_idx": start_idx,
-                "end_idx": end_idx,
-                "parent_header": parent_header
-            }))
+        for chunk in chunks:
+            header_doc_copy = header_doc.copy()
+            header_doc_copy.pop("doc_index")
 
-        chunks_with_info = [{
-            "chunk_idx": chunk_idx,
-            "tokens": count_tokens(config["embed_model"], chunk),
-            "chunk": chunk,
-        } for chunk_idx, chunk in enumerate(chunks_with_headers)]
+            header = header_doc_copy.pop("header")
+            parent_header = header_doc_copy.pop("parent_header")
+            parent_headers = header_doc_copy.pop("parent_headers")
+
+            source = header_doc_copy.pop("source")
+            tokens = header_doc_copy.pop("tokens")
+
+            chunk_copy = chunk.copy()
+            header_doc_copy["id"] = chunk_copy.pop("id")
+            chunk_content = chunk_copy.pop("content")
+
+            text = f"{header}\n{chunk_content}"
+
+            merged_data = {
+                **header_doc_copy,
+                "text": text,
+                "metadata": {
+                    **chunk_copy,
+                    "source": source,
+                    "parent_headers": parent_headers,
+                    "parent_header": parent_header,
+                    "header": header,
+                    "content": chunk_content,
+                    # "tokens": tokens,
+                }
+            }
+            chunks_with_metadata.append(merged_data)
+            all_chunks_with_metadata.append(merged_data)
+
         chunked_docs.append({
             "doc_index": doc_index,
-            "count": len(chunks_with_info),
-            "total_tokens": sum(chunk_info["tokens"] for chunk_info in chunks_with_info),
-            "chunks": chunks_with_info,
+            "count": len(chunks_with_metadata),
+            "total_tokens": sum(chunk["metadata"]["num_tokens"] for chunk in chunks_with_metadata),
+            "chunks": chunks_with_metadata,
         })
 
-    token_counts: List[int] = count_tokens(config["embed_model"], [
-                                           text for text, _ in texts_with_metadata], prevent_total=True)
+    save_file({
+        "query": query,
+        "model": embed_model,
+        "count": len(all_chunks_with_metadata),
+        "total_tokens": sum(chunk["metadata"]["num_tokens"] for chunk in all_chunks_with_metadata),
+        "chunks": all_chunks_with_metadata
+    }, f"{OUTPUT_DIR}/all_chunks_with_metadata.json")
+
+    token_counts = [chunk["metadata"]["num_tokens"]
+                    for chunk in all_chunks_with_metadata]
     texts_with_tokens = [{
         "tokens": tokens,
-        "text": text,
-    } for tokens, (text, _) in zip(token_counts, texts_with_metadata)]
+        "text": chunk["text"],
+    } for tokens, chunk in zip(token_counts, all_chunks_with_metadata)]
     save_file({
-        "count": len(texts_with_metadata),
+        "count": len(all_chunks_with_metadata),
         "max_tokens": max(token_counts),
         "ave_tokens": math.ceil(sum(token_counts) / len(token_counts)) if token_counts else 0,
         "min_tokens": min(token_counts),
@@ -214,11 +240,29 @@ def main():
         "chunks": chunked_docs
     }, f"{OUTPUT_DIR}/chunked_docs.json")
 
+    all_texts = [
+        chunk["text"]
+        for chunk in all_chunks_with_metadata
+    ]
+    all_ids = [chunk["id"] for chunk in all_chunks_with_metadata]
+    token_counts_all_texts: List[int] = count_tokens(
+        embed_model, all_texts, prevent_total=True)
+    all_texts_for_clustering = [{
+        "tokens": tokens,
+        "text": text,
+    } for tokens, text in zip(token_counts_all_texts, all_texts)]
+    save_file({
+        "query": query,
+        "model": llm_model,
+        "count": len(all_texts_for_clustering),
+        "total_tokens": sum(d["tokens"] for d in all_texts_for_clustering),
+        "texts": all_texts_for_clustering
+    }, f"{OUTPUT_DIR}/all_texts_for_clustering.json")
+
     retriever = VectorRetriever(config)
-    retriever.load_or_compute_embeddings(texts_with_metadata)
+    retriever.load_or_compute_embeddings(all_texts, ids=all_ids)
     retriever.cluster_embeddings()
     retriever.build_index()
-    search_chunk_results = retriever.search_chunks(query)
 
     # Save cluster assignments to clusters.json
     clusters = retriever.get_clusters()
@@ -241,6 +285,7 @@ def main():
         "centroids": search_centroid_results
     }, f"{OUTPUT_DIR}/search_centroid_results.json")
 
+    search_chunk_results = retriever.search_chunks(query)
     save_file({
         "query": query,
         "count": len(search_chunk_results),
