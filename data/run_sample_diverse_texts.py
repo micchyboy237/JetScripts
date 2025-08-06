f236638c899691a5b94d1f6b9ba1ce04e371d3de
Jethro Estrada <jethroestrada237@gmail.com>
2025-08-06 20:36:21 +0800

    Updates

--- data/run_sample_diverse_texts.py
+++ data/run_sample_diverse_texts.py
@@ -9,6 +9,7 @@ from jet.logger import logger
 from jet.models.model_types import EmbedModelType
 from jet.models.tokenizer.base import count_tokens
 from jet.transformers.formatters import format_json
+from jet.wordnet.n_grams import get_most_common_ngrams
 from jet.wordnet.text_chunker import chunk_texts_with_data
 
 OUTPUT_DIR = os.path.join(
@@ -17,35 +18,25 @@ shutil.rmtree(OUTPUT_DIR, ignore_errors=True)
 
 
 if __name__ == "__main__":
-    docs_file = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/features/generated/run_search_and_rerank_4/top_isekai_anime_2025/docs.json"
+    texts = [
+        "Machine learning is a method of data analysis that automates model building.",
+        "Deep learning, a subset of machine learning, uses neural networks for complex tasks.",
+        "Supervised learning involves training models on labeled datasets.",
+        "Unsupervised learning finds patterns in unlabeled data using clustering.",
+        "Python is a popular programming language for machine learning development.",
+        "Reinforcement learning optimizes decisions through trial and error.",
+        "Data preprocessing is critical for effective machine learning models."
+    ]
 
-    embed_model: EmbedModelType = "all-MiniLM-L6-v2"
-    chunk_size = 500
-    chunk_overlap = 100
+    all_common_ngrams = get_most_common_ngrams(texts, min_words=1)
+    save_file(all_common_ngrams, f"{OUTPUT_DIR}/all_common_ngrams.json")
 
-    header_docs: List[HeaderDoc] = load_file(docs_file)["documents"]
-
-    chunk_texts = []
-    for header_doc in header_docs:
-        doc_index = header_doc["doc_index"]
-        header = header_doc["header"]
-        content = header_doc['content']
-        source = header_doc.get("source", "Unknown Source")
-        parent_header = header_doc.get("parent_header", None)
-        buffer: int = count_tokens(embed_model, header)
-        doc_ids = [header_doc["id"]]
-        chunks = chunk_texts_with_data(content, chunk_size,
-                                       chunk_overlap, embed_model, doc_ids, buffer)
-        chunk_texts.extend([
-            f"{header}\n{chunk["content"]}"
-            for chunk in chunks
-        ])
-
-    save_file(chunk_texts, f"{OUTPUT_DIR}/chunk_texts.json")
-
-    results: List[str] = sample_diverse_texts(chunk_texts)
+    results: List[str] = sample_diverse_texts(texts)
 
     logger.gray(f"Results: ({len(results)})")
     logger.success(format_json(results))
+
+    result_common_ngrams = get_most_common_ngrams(results, min_words=1)
+    save_file(result_common_ngrams, f"{OUTPUT_DIR}/result_common_ngrams.json")
 
     save_file(results, f"{OUTPUT_DIR}/results.json")
