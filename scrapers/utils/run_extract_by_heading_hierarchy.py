6ea1ae4f6758ada6f0bdab81034b48157bd518a0
Jethro Estrada <jethroestrada237@gmail.com>
2025-08-07 09:44:51 +0800

    Updates

--- scrapers/utils/run_extract_by_heading_hierarchy.py
+++ scrapers/utils/run_extract_by_heading_hierarchy.py
@@ -15,12 +15,21 @@ if __name__ == "__main__":
     html_str: str = load_file(html_file)
     save_file(html_str, f"{output_dir}/page.html")
 
-    headings = extract_by_heading_hierarchy(html_str)
-    save_file(headings, f"{output_dir}/headings.json")
+    header_docs = extract_by_heading_hierarchy(html_str)
+    save_file(header_docs, f"{output_dir}/header_docs.json")
 
-    headings_html_strings = [node.get_html() for node in headings]
+    headers = [node.header for node in header_docs]
+    save_file(headers, f"{output_dir}/headers.json")
+
+    contents = [node.content for node in header_docs]
+    save_file(contents, f"{output_dir}/contents.json")
+
+    texts = [node.text for node in header_docs]
+    save_file(texts, f"{output_dir}/texts.json")
+
+    headings_html_strings = [node.get_html() for node in header_docs]
     save_file(headings_html_strings,
               f"{output_dir}/headings_html_strings.json")
 
-    heading_parents = [node.get_parent_node() for node in headings]
+    heading_parents = [node.get_parent_node() for node in header_docs]
     save_file(heading_parents, f"{output_dir}/heading_parents.json")
