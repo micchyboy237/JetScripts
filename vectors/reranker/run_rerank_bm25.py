0d932f31313e0ca2db910b53110c055d59bc4ad4
Jethro Estrada <jethroestrada237@gmail.com>
2025-08-16 20:47:40 +0800

    Updates

---
+++ vectors/reranker/run_rerank_bm25.py
@@ -0,0 +1,22 @@
+import os
+from jet.file.utils import load_file, save_file
+from jet.vectors.reranker.bm25 import rerank_bm25
+
+OUTPUT_DIR = os.path.join(os.path.dirname(
+    __file__), "generated", os.path.splitext(os.path.basename(__file__))[0])
+
+if __name__ == "__main__":
+    docs_file = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/vectors/semantic_search/generated/run_file_vector_search/results_merged.json"
+    docs = load_file(docs_file)["results"][:50]
+
+    query = "RAG agents"
+    texts = [doc["code"] for doc in docs]
+
+    query_candidates, reranked_results = rerank_bm25(query, texts)
+
+    save_file({
+        "query": query,
+        "candidates": query_candidates,
+        "count": len(reranked_results),
+        "results": reranked_results
+    }, f"{OUTPUT_DIR}/example/reranked_results.json")
