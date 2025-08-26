441ac81a82bc09e199dec37d87cba8c1d1dac93e
Jethro Estrada <jethroestrada237@gmail.com>
2025-08-27 04:11:38 +0800

    Updates

--- executor/run_python_files_in_directory.py
+++ executor/run_python_files_in_directory.py
@@ -1,9 +1,9 @@
 from jet.executor.python_runner import run_python_files_in_directory
 
 if __name__ == "__main__":
-    includes = ["*21_*", "*best_*",]
-    excludes = ["*18_*"]
+    includes = []
+    excludes = []
     run_python_files_in_directory(
-        "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/converted_doc_scripts/all-rag-techniques",
+        "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/converted_doc_scripts/GenAI_Agents/converted-notebooks/all_agents_tutorials",
         includes=includes,
         excludes=excludes)
