70a798892e10c69acb61bc9b989a4d3619225519
Jethro Estrada <jethroestrada237@gmail.com>
2025-08-27 04:18:52 +0800

    Updates

--- executor/run_python_files_in_directory.py
+++ executor/run_python_files_in_directory.py
@@ -1,4 +1,10 @@
+import os
+import shutil
 from jet.executor.python_runner import run_python_files_in_directory
+
+OUTPUT_DIR = os.path.join(
+    os.path.dirname(__file__), "generated", os.path.splitext(os.path.basename(__file__))[0])
+shutil.rmtree(OUTPUT_DIR, ignore_errors=True)
 
 if __name__ == "__main__":
     includes = []
@@ -6,4 +12,6 @@ if __name__ == "__main__":
     run_python_files_in_directory(
         "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/converted_doc_scripts/GenAI_Agents/converted-notebooks/all_agents_tutorials",
         includes=includes,
-        excludes=excludes)
+        excludes=excludes,
+        output_dir=OUTPUT_DIR,
+    )
