96567c6e975a4d1f84c30bca2ddb99f98dd562bb
Jethro Estrada <jethroestrada237@gmail.com>
2025-09-07 23:03:04 +0800

    Updates

--- converted_doc_scripts/run_extract_notebook_texts.py
+++ converted_doc_scripts/run_extract_notebook_texts.py
@@ -1,7 +1,7 @@
 from jet.code.extraction.extract_notebook_texts import run_text_extraction
 
 if __name__ == "__main__":
-    input_path = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/converted_doc_scripts/all-rag-techniques/notebooks"
-    output_dir = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/converted_doc_scripts/all-rag-techniques/docs_text_only"
+    input_path = "/Users/jethroestrada/Desktop/External_Projects/AI/repo-libs/swarms"
+    output_dir = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/jet_python_modules/jet/libs/swarms/notebook_texts"
 
     run_text_extraction(input_path, output_dir)
