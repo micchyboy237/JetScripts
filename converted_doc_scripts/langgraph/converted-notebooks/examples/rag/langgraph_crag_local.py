1fa2743251ead1a5097dfffc3d70f47405fb53e4
Jethro Estrada <jethroestrada237@gmail.com>
2025-09-10 15:46:36 +0800

    Updates

--- converted_doc_scripts/langgraph/converted-notebooks/examples/rag/langgraph_crag_local.py
+++ converted_doc_scripts/langgraph/converted-notebooks/examples/rag/langgraph_crag_local.py
@@ -6,7 +6,7 @@ from langchain import hub
 from langchain.prompts import PromptTemplate
 from langchain.schema import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.chat_models import ChatOllama
+# from langchain_community.chat_models import ChatOllama
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_community.vectorstores import SKLearnVectorStore
@@ -176,17 +176,20 @@ doc_txt = docs[1].page_content
 logger.debug(retrieval_grader.invoke({"question": question, "documents": doc_txt}))
 
 
+# Update retrieval_grader prompt
 prompt = PromptTemplate(
-    template="""You are an assistant for question-answering tasks.
-
-    Use the following documents to answer the question.
-
-    If you don't know the answer, just say that you don't know.
-
-    Use three sentences maximum and keep the answer concise:
-    Question: {question}
-    Documents: {documents}
-    Answer:
+    template="""You are a teacher grading a quiz. You will be given:
+    1/ a QUESTION
+    2/ A FACT provided by the student
+    You are grading RELEVANCE RECALL:
+    A score of 1 means that ANY of the statements in the FACT are relevant to the QUESTION.
+    A score of 0 means that NONE of the statements in the FACT are relevant to the QUESTION.
+    1 is the highest (best) score. 0 is the lowest score you can give.
+    Explain your reasoning in a step-by-step manner internally, but do not include the explanation in the output.
+    Question: {question} \n
+    Fact: \n\n {documents} \n\n
+    Provide the binary score as a JSON object with a single key 'score' containing 'yes' or 'no'. Return only the JSON object, with no additional text, explanation, or preamble.
+    Example output: {"score": "yes"}
     """,
     input_variables=["question", "documents"],
 )
