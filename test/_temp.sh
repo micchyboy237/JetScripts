d8950d7897770c8a4bd7e5770915849eb31aa4c1
Jethro Estrada <jethroestrada237@gmail.com>
2025-09-20 17:33:44 +0800

    Updates

--- test/_temp.sh
+++ test/_temp.sh
@@ -6,3 +6,15 @@ llama-server --jinja -fa on -hf bartowski/Qwen2.5-7B-Instruct-GGUF:Q4_K_M --host
 llama-server --jinja -fa on -hf bartowski/Mistral-Nemo-Instruct-2407-GGUF:Q4_K_M --host 0.0.0.0 --port 8080
 llama-server --jinja -fa on -hf bartowski/DeepSeek-R1-Distill-Qwen-7B-GGUF:Q4_K_M --chat-template-file models/templates/llama-cpp-deepseek-r1.jinja --host 0.0.0.0 --port 8080
 llama-server --jinja -fa on -hf bartowski/Hermes-3-Llama-3.1-8B-GGUF:Q4_K_M --chat-template-file models/templates/NousResearch-Hermes-3-Llama-3.1-8B-tool_use.jinja --host 0.0.0.0 --port 8080
+
+# Chat CURL command
+curl -N http://shawn-pc.local:8080/v1/chat/completions \
+  -H "Content-Type: application/json" \
+  -d '{
+    "model": "bartowski/DeepSeek-R1-Distill-Qwen-7B-GGUF:Q4_K_M",
+    "messages": [
+      {"role": "system", "content": "You are a helpful assistant."},
+      {"role": "user", "content": "Explain black holes in simple terms."}
+    ],
+    "stream": true
+  }'
