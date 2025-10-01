f9ca4ba1fdae8f1a07f71e9b6ba5085dec2628a3
Jethro Estrada <jethroestrada237@gmail.com>
2025-10-02 02:03:56 +0800

    Updates

--- converted_doc_scripts/stanza/converted-notebooks/examples/CONLL_Dependency_Visualizer_Example.py
+++ converted_doc_scripts/stanza/converted-notebooks/examples/CONLL_Dependency_Visualizer_Example.py
@@ -18,21 +18,21 @@ en_file = f"{os.path.dirname(__file__)}/data/en_test.conllu.txt"
 
 html_strings = conll_to_visual(en_file, "en", sent_count=2)
 for i, html in enumerate(html_strings):
-  save_file(html, f"{OUTPUT_DIR}/en/html_sent_count_2.html")
+  save_file(html, f"{OUTPUT_DIR}/en/html_sent_count_2_{i + 1}.html")
 html_strings = conll_to_visual(en_file, "en", sent_count=10)
 for i, html in enumerate(html_strings):
-  save_file(html, f"{OUTPUT_DIR}/en/html_sent_count_10.html")
+  save_file(html, f"{OUTPUT_DIR}/en/html_sent_count_10_{i + 1}.html")
 
 
 jp_file = f"{os.path.dirname(__file__)}/data/japanese_test.conllu.txt"
 html_strings = conll_to_visual(jp_file, "ja")
 for i, html in enumerate(html_strings):
-  save_file(html, f"{OUTPUT_DIR}/ja/html_japanese_test.html")
+  save_file(html, f"{OUTPUT_DIR}/ja/html_japanese_test_{i + 1}.html")
 
 
 ar_file = f"{os.path.dirname(__file__)}/data/arabic_test.conllu.txt"
 html_strings = conll_to_visual(ar_file, "ar")
 for i, html in enumerate(html_strings):
-  save_file(html, f"{OUTPUT_DIR}/ar/html_arabic_test.html")
+  save_file(html, f"{OUTPUT_DIR}/ar/html_arabic_test_{i + 1}.html")
 
 logger.info("\n\n[DONE]", bright=True)
