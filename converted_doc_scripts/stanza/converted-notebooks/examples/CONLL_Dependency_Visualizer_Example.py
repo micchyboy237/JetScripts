06977e9f9be9fe0c2f1e043782cd1163994dd363
Jethro Estrada <jethroestrada237@gmail.com>
2025-10-01 23:19:59 +0800

    Updates

--- converted_doc_scripts/stanza/converted-notebooks/examples/CONLL_Dependency_Visualizer_Example.py
+++ converted_doc_scripts/stanza/converted-notebooks/examples/CONLL_Dependency_Visualizer_Example.py
@@ -13,17 +13,17 @@ logger.basicConfig(filename=log_file)
 logger.info(f"Logs: {log_file}")
 
 
-en_file = "en_test.conllu.txt"
+en_file = f"{os.path.dirname(__file__)}/data/en_test.conllu.txt"
 
 conll_to_visual(en_file, "en", sent_count=2)
 conll_to_visual(en_file, "en", sent_count=10)
 
 
-jp_file = "japanese_test.conllu.txt"
+jp_file = f"{os.path.dirname(__file__)}/data/japanese_test.conllu.txt"
 conll_to_visual(jp_file, "ja")
 
 
-ar_file = "arabic_test.conllu.txt"
+ar_file = f"{os.path.dirname(__file__)}/data/arabic_test.conllu.txt"
 conll_to_visual(ar_file, "ar")
 
 logger.info("\n\n[DONE]", bright=True)
