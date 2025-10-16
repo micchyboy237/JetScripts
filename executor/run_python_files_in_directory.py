0b080b02d291b166ef5bb1baf30f5d1ad9bb5f29
Jethro Estrada <jethroestrada237@gmail.com>
2025-10-16 08:44:03 +0800

    Updates

--- executor/run_python_files_in_directory.py
+++ executor/run_python_files_in_directory.py
@@ -1,32 +1,25 @@
 from typing import Literal
 from jet.executor.python_runner import run_python_files_in_directory
-from jet.utils.file import search_files
-
-def filter_files(base_dir):
-    include_contents = ["*if __name__ == *"]
-    excludes = []
-    extensions = [".py"]
-    results = search_files(
-        base_dir,
-        extensions,
-        exclude_files=excludes,
-        include_contents=include_contents,
-    )
-    return results
 
 if __name__ == "__main__":
     target_dir = "/Users/jethroestrada/Desktop/External_Projects/AI/repo-libs/stanza"
     output_dir = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/libs/stanza/generated/all_files_with_main_block/runner_status"
-    includes = filter_files(target_dir)
+
+    includes = []
+    include_contents = ["*if __name__ == *"]
     excludes = [
         # "ambiguity-clarity.py",
     ]
+    exclude_contents = []
     rerun_mode: Literal["all", "failed",
-                        "unrun", "failed_and_unrun"] = "failed"
+                        "unrun", "failed_and_unrun"] = "all"
+
     run_python_files_in_directory(
         target_dir,
         includes=includes,
         excludes=excludes,
+        include_contents=include_contents,
+        exclude_contents=exclude_contents,
         output_dir=output_dir,
         rerun_mode=rerun_mode,
         recursive=True,
