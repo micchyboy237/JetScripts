612f6deb924dfc606cdea6279b395ffdd3d758e8
Jethro Estrada <jethroestrada237@gmail.com>
2025-10-20 11:30:12 +0800

    Updates

--- code/markdown_utils/run_remove_markdown_links.py
+++ code/markdown_utils/run_remove_markdown_links.py
@@ -13,6 +13,10 @@ With [Go to home](/) text
 [ ](https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fgamerant.com%2Fnew-isekai-anime-2025%2F%2F&src=sdkpreparse) [ ](https://twitter.com/intent/tweet?text=Every%20New%20Isekai%20Anime%20Announced%20For%202025%20%28So%20Far%29&url=https%3A%2F%2Fgamerant.com%2Fnew-isekai-anime-2025%2F) [ ](https://web.whatsapp.com/send?text=https%3A%2F%2Fgamerant.com%2Fnew-isekai-anime-2025%2F)
 
 [ ](https://www.linkedin.com/shareArticle?mini=true&url=https%3A%2F%2Fgamerant.com%2Fnew-isekai-anime-2025%2F&title=Every%20New%20Isekai%20Anime%20Announced%20For%202025%20%28So%20Far%29&source=gamerant.com&summary=Isekai%20anime%20is%20inescapable%2C%20with%20each%20season%20containing%20a%20couple%20of%20shows.%20Here%20are%20the%202025%20isekai%20anime%20series%20announced%20so%20far.) [ ](https://bsky.app/intent/compose?text=https%3A%2F%2Fgamerant.com%2Fnew-isekai-anime-2025%2F) [ ](https://www.reddit.com/submit?url=https%3A%2F%2Fgamerant.com%2Fnew-isekai-anime-2025%2F) [ ](http://share.flipboard.com/bookmarklet/popout?v=2&title=Every%20New%20Isekai%20Anime%20Announced%20For%202025%20%28So%20Far%29&url=https%3A%2F%2Fgamerant.com%2Fnew-isekai-anime-2025%2F&utm_campaign=tools&utm_medium=article-share&utm_source=gamerant.com)
+
+[ ](mailto:?Subject=Every New Isekai Anime Announced For 2025 \(So Far\)&Body=Check%20this%20out%21%0Ahttps%3A%2F%2Fgamerant.com%2Fnew-isekai-anime-2025%2F)
+
+Facebook X LinkedIn Reddit Flipboard Copy link [ Email ](mailto:?Subject=Every New Isekai Anime Announced For 2025 \(So Far\)&Body=Check%20this%20out%21%0Ahttps://gamerant.com/new-isekai-anime-2025/)
 """
 
 if __name__ == "__main__":
