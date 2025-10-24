aba1119e4303e5dac42b74e61e6cc5b98e1a8d3b
Jethro Estrada <jethroestrada237@gmail.com>
2025-10-24 18:27:01 +0800

    Updates

--- models/run_get_model_context_embed_size.py
+++ models/run_get_model_context_embed_size.py
@@ -1,13 +1,12 @@
 import os
 from jet.file.utils import save_file
 from jet.models.model_types import ModelType
-from jet.models.utils import get_model_info, get_model_limits
+from jet.models.utils import get_model_limits
 from jet.logger import logger
-from jet.transformers.formatters import format_json
 
 if __name__ == "__main__":
     output_dir = os.path.join(os.path.dirname(__file__), "constants")
-    model_id: ModelType = "mistralai/Mistral-7B-Instruct-v0.3"
+    model_id: ModelType = "Qwen/Qwen3-4B-Instruct-2507"
 
     max_context, max_embeddings = get_model_limits(model_id)
 
