3338ad9161eeeb595e5b1d0550e476f7d0f53141
Jethro Estrada <jethroestrada237@gmail.com>
2025-11-01 12:43:38 +0800

    Updates

--- test/_temp_test5.py
+++ test/_temp_test5.py
@@ -1,28 +1,27 @@
-import speech_recognition as sr
+# import spacy
+# from spacy import displacy
+# from spacy.tokens import Span
+
+# text = "Welcome to the Bank of China."
+
+# nlp = spacy.blank("en")
+# doc = nlp(text)
+
+# doc.spans["sc"] = [
+#     Span(doc, 3, 6, "ORG"),
+#     Span(doc, 5, 6, "GPE"),
+# ]
+
+# displacy.serve(doc, style="span", port=5001)
 
 
-def recognize_speech_from_mic():
-    # Initialize recognizer class (for recognizing speech)
-    recognizer = sr.Recognizer()
+import webbrowser
+import spacy
+from spacy import displacy
 
-    # Use the default microphone as the audio source
-    with sr.Microphone() as source:
-        print("Please say something:")
-        # Adjust for ambient noise to improve recognition accuracy
-        recognizer.adjust_for_ambient_noise(source)
-        # Listen for the first phrase and extract it into audio data
-        audio = recognizer.listen(source)
+text = "When Sebastian Thrun started working on self-driving cars at Google in 2007, few people outside of the company took him seriously."
 
-    try:
-        # Recognize speech using Google's free Web Speech API
-        text = recognizer.recognize_google(audio)
-        print("You said: " + text)
-        return text
-    except sr.UnknownValueError:
-        print("Sorry, I could not understand the audio.")
-    except sr.RequestError as e:
-        print(f"Could not request results from the service; {e}")
-
-
-if __name__ == "__main__":
-    recognize_speech_from_mic()
+nlp = spacy.load("en_core_web_sm")
+doc = nlp(text)
+webbrowser.open('http://localhost:5001')
+displacy.serve(doc, style="ent", port=5001)
