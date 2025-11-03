5af35dc58cf08575870f04b97afdd5553878f70e
Jethro Estrada <jethroestrada237@gmail.com>
2025-11-03 23:45:14 +0800

    Updates

--- test/_temp_test6.py
+++ test/_temp_test6.py
@@ -1,7 +1,9 @@
+import os
+from pathlib import Path
 from jet.scrapers.browser.config import PLAYWRIGHT_CHROMIUM_EXECUTABLE
 from playwright.sync_api import sync_playwright
 
-def main():
+def example_page_evaluate():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True, executable_path=PLAYWRIGHT_CHROMIUM_EXECUTABLE)
         page = browser.new_page()
@@ -30,5 +32,24 @@ def main():
 
         browser.close()
 
+def example_inject_js():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(executable_path=PLAYWRIGHT_CHROMIUM_EXECUTABLE)
+        page = browser.new_page()
+        page.goto("https://example.com")
+
+        current_dir = os.path.dirname(__file__)
+        js_path = Path(os.path.join(current_dir, "scripts/utils.js")).resolve()
+        page.add_script_tag(path=str(js_path))
+
+        bbox = page.evaluate("Utils.getBoundingBox('h1')")
+        print("From injected bounding box:", bbox)
+
+        message = page.evaluate("Utils.myInjectedFunction('Jet')")
+        print(message)
+
+        browser.close()
+
 if __name__ == "__main__":
-    main()
+    example_page_evaluate()
+    example_inject_js()
