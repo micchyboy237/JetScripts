826b89da8c1cfc7a9c15f47fc6214d1d5e50b5ec
Jethro Estrada <jethroestrada237@gmail.com>
2025-11-03 19:43:38 +0800

    Updates

--- test/ner/run_span_marker_ontonotes5.py
+++ test/ner/run_span_marker_ontonotes5.py
@@ -15,24 +15,16 @@ text = """Title: Headhunted to Another World: From Salaryman to Big Four!
 Isekai
 Fantasy
 Comedy
-Release Date : January 1, 2025
-Release Date
-: January 1, 2025
+Release Date: January 1, 2025
 Japanese Title : Salaryman ga Isekai ni Ittara Shitennou ni Natta Hanashi Studio : Geek Toys, CompTown Based On : Manga Creator : Benigashira Streaming Service(s) : Crunchyroll
-Japanese Title
-: Salaryman ga Isekai ni Ittara Shitennou ni Natta Hanashi
 
-Studio
-: Geek Toys, CompTown
+Studio: Geek Toys, CompTown
 
-Based On
-: Manga
+Based On: Manga
 
-Creator
-: Benigashira
+Creator: Benigashira
 
-Streaming Service(s)
-: Crunchyroll
+Streaming Service(s): Crunchyroll
 Powered by
 Expand Collapse
 Plenty of 2025 isekai anime will feature OP protagonists capable of brute-forcing their way through any and every encounter, so it is always refreshing when an MC comes along that relies on brain rather than brawn. A competent office worker who feels underappreciated, Uchimura is suddenly summoned to another world by a demonic ruler, who comes with quite an unusual offer: Join the crew as one of the Heavenly Kings. So, Uchimura starts a new career path that tasks him with tackling challenges using his expertise in discourse and sales.
