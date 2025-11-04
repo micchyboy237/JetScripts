fc562be96fe544cec6286b082205c8d8eeace7e4
Jethro Estrada <jethroestrada237@gmail.com>
2025-11-04 21:38:37 +0800

    Updates

--- test/ner/run_span_marker_ontonotes5.py
+++ test/ner/run_span_marker_ontonotes5.py
@@ -1,5 +1,6 @@
 import os
 from jet.file.utils import save_file
+from jet.libs.bertopic.examples.mock import load_sample_data
 from span_marker import SpanMarkerModel
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
@@ -11,30 +12,10 @@ model = SpanMarkerModel.from_pretrained(
 ).to("mps")
 
 # Example text
-text = """Title: Headhunted to Another World: From Salaryman to Big Four!
-Isekai
-Fantasy
-Comedy
-Release Date: January 1, 2025
-Japanese Title: Salaryman ga Isekai ni Ittara Shitennou ni Natta Hanashi Studio
-
-Studio: Geek Toys, CompTown
-
-Based On: Manga
-
-Creator: Benigashira
-
-Streaming Service(s): Crunchyroll
-Powered by
-Expand Collapse
-Plenty of 2025 isekai anime will feature OP protagonists capable of brute-forcing their way through any and every encounter, so it is always refreshing when an MC comes along that relies on brain rather than brawn. A competent office worker who feels underappreciated, Uchimura is suddenly summoned to another world by a demonic ruler, who comes with quite an unusual offer: Join the crew as one of the Heavenly Kings. So, Uchimura starts a new career path that tasks him with tackling challenges using his expertise in discourse and sales.
-Related"""
+texts = load_sample_data()
 
 # Predict entities
-entities = model.predict(text)
-
-print(entities)
-
+entities = model.predict(texts, batch_size=32, show_progress_bar=True)
 
 output_dir = os.path.join(os.path.dirname(
     __file__), "generated", os.path.splitext(os.path.basename(__file__))[0])
