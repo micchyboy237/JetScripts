58ad1bcaf897af9182d0fcb47aefdba909740ed8
Jethro Estrada <jethroestrada237@gmail.com>
2025-11-04 20:47:08 +0800

    Updates

--- wordnet/analyzers/run_analyze_pos_tags.py
+++ wordnet/analyzers/run_analyze_pos_tags.py
@@ -9,18 +9,19 @@ if __name__ == '__main__':
 
     texts = load_sample_data()
     texts = [{"text": t, "lang": "en"} for t in texts]
-    includes_pos = ['PROPN', 'NOUN']
+    includes_pos = ['PROPN', 'NOUN', 'ADJ', 'VERB', 'ADV']
+    top_n = 20
 
     analyze_pos_tags(texts, n=2, from_start=True,
-                     words_only=True, includes_pos=includes_pos, output_dir=output_dir)
+                     words_only=True, top_n=top_n, includes_pos=includes_pos, output_dir=output_dir)
     analyze_pos_tags(texts, n=3, from_start=True,
-                     words_only=True, includes_pos=includes_pos, output_dir=output_dir)
+                     words_only=True, top_n=top_n, includes_pos=includes_pos, output_dir=output_dir)
     excludes_pos = ['[BOS]', '[EOS]']
     analyze_pos_tags(texts, n=1, from_start=False,
-                     words_only=True, includes_pos=includes_pos, output_dir=output_dir)
+                     words_only=True, top_n=top_n, includes_pos=includes_pos, output_dir=output_dir)
     analyze_pos_tags(texts, n=2, from_start=False,
-                     words_only=True, includes_pos=includes_pos, output_dir=output_dir)
+                     words_only=True, top_n=top_n, includes_pos=includes_pos, output_dir=output_dir)
     analyze_pos_tags(texts, n=1, from_start=False,
-                     excludes_pos=excludes_pos, includes_pos=includes_pos, output_dir=output_dir)
+                     excludes_pos=excludes_pos, top_n=top_n, includes_pos=includes_pos, output_dir=output_dir)
     analyze_pos_tags(texts, n=2, from_start=False,
-                     excludes_pos=excludes_pos, includes_pos=includes_pos, output_dir=output_dir)
+                     excludes_pos=excludes_pos, top_n=top_n, includes_pos=includes_pos, output_dir=output_dir)
