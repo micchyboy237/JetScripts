bd09ffabe00ed0725b6440f29b3252c796714ce3
Jethro Estrada <jethroestrada237@gmail.com>
2025-12-03 07:20:57 +0800

    Updates

--- audio/transcribers/run_transcribe_audio.py
+++ audio/transcribers/run_transcribe_audio.py
@@ -84,7 +84,7 @@ def translate_audio_files(
                 word_timestamps=word_timestamps,
                 chunk_length=chunk_length,
                 without_timestamps=False,
-                condition_on_previous_text=True,
+                condition_on_previous_text=False,
                 log_progress=True
             )
 
@@ -127,8 +127,8 @@ def translate_audio_files(
 # ==============================
 if __name__ == "__main__":
     example_files = [
-        "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/jet_python_modules/jet/audio/speech/silero/generated/silero_vad_stream",
-        # "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/jet_python_modules/jet/audio/speech/silero/generated/silero_vad_stream/segment_001/sound.wav",
+        # "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/jet_python_modules/jet/audio/speech/silero/generated/silero_vad_stream",
+        "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/audio/generated/run_stream_device_output",
         # "/Users/jethroestrada/Desktop/External_Projects/Jet_Windows_Workspace/python_scripts/samples/data/audio/1.wav",
     ]
 
