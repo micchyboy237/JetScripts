78a353823701b53261c04b5156f8261702edc561
Jethro Estrada <jethroestrada237@gmail.com>
2026-01-03 18:14:21 +0800

    Updates

--- models/download_readme.py
+++ models/download_readme.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     overwrite = True
     extract_code = True
     hf_readme_download_dir = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/jet_python_modules/docs/hf_readmes"
-    model: ModelType = "litagin/anime-whisper"
+    model: ModelType = "FunAudioLLM/SenseVoiceSmall"
 
     model_value: ModelValue = resolve_model_value(model)
     download_readme(model_value, hf_readme_download_dir,
