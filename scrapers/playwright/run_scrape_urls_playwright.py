b3845fe9ce653659ab533700bebbecdec61946d8
Jethro Estrada <jethroestrada237@gmail.com>
2026-02-28 13:33:36 +0800

    Updates

--- scrapers/playwright/run_scrape_urls_playwright.py
+++ scrapers/playwright/run_scrape_urls_playwright.py
@@ -1,18 +1,23 @@
 import os
 import shutil
 import time
+from pathlib import Path
 from typing import List, Literal, Optional, TypedDict
 
+from jet.code.markdown_utils import (
+    analyze_markdown,
+    base_parse_markdown,
+    convert_html_to_markdown,
+    derive_by_header_hierarchy,
+)
+from jet.code.markdown_utils._preprocessors import extract_markdown_links
+from jet.file.utils import save_file
 from jet.logger import logger
 from jet.scrapers.playwright_utils import scrape_urls, scrape_urls_sync
 from jet.scrapers.utils import scrape_links
-from jet.utils.text import format_sub_dir
+from jet.utils.text import format_sub_dir, format_sub_source_dir
 
-OUTPUT_DIR = os.path.join(
-    os.path.dirname(__file__),
-    "generated",
-    os.path.splitext(os.path.basename(__file__))[0],
-)
+OUTPUT_DIR = Path(__file__).parent / "generated" / Path(__file__).stem
 shutil.rmtree(OUTPUT_DIR, ignore_errors=True)
 
 log_file = os.path.join(OUTPUT_DIR, "main.log")
@@ -79,13 +84,6 @@ async def async_example(urls: List[str]) -> None:
 
 
 def sync_example(urls: List[str]) -> None:
-    sub_dir = f"{OUTPUT_DIR}/sync_results"
-
-    screenshots_dir = os.path.join(sub_dir, "screenshots")
-    html_files_dir = os.path.join(sub_dir, "html_files")
-    os.makedirs(screenshots_dir, exist_ok=True)
-    os.makedirs(html_files_dir, exist_ok=True)
-
     html_list = []
 
     start = time.perf_counter()
@@ -105,28 +103,39 @@ def sync_example(urls: List[str]) -> None:
     )
 
     for result in results:
-        if result["status"] == "completed" and result["html"]:
-            all_links = scrape_links(result["html"], base_url=result["url"])
-            safe_filename = format_sub_dir(result["url"])
+        html = result["html"]
+        if result["status"] == "completed" and html:
+            url = result["url"]
+            sub_output_dir = OUTPUT_DIR / format_sub_source_dir(url) / "sync_results"
+            sub_output_dir.mkdir(parents=True, exist_ok=True)
+
+            save_file({"url": url}, sub_output_dir / "input.json")
+            save_file(html, sub_output_dir / "page.html")
+
+            doc_markdown = convert_html_to_markdown(html, ignore_links=False)
+            save_file(doc_markdown, f"{sub_output_dir}/page.md")
+            doc_analysis = analyze_markdown(doc_markdown)
+            save_file(doc_analysis, f"{sub_output_dir}/analysis.json")
+            doc_markdown_tokens = base_parse_markdown(doc_markdown)
+            save_file(doc_markdown_tokens, f"{sub_output_dir}/markdown_tokens.json")
+            original_docs = derive_by_header_hierarchy(doc_markdown, ignore_links=False)
+            save_file(original_docs, f"{sub_output_dir}/docs.json")
+
+            # links = scrape_links(html, base_url=url)
+            links, _ = extract_markdown_links(
+                doc_markdown, base_url=url, ignore_links=True
+            )
+            save_file(links, sub_output_dir / "links.json")
 
             if result["screenshot"]:
-                screenshot_path = os.path.join(screenshots_dir, f"{safe_filename}.png")
-                with open(screenshot_path, "wb") as f:
-                    f.write(result["screenshot"])
-                logger.success(
-                    f"Scraped {result['url']}, links count: {len(all_links)}, "
-                    f"screenshot saved: {screenshot_path}"
-                )
+                save_file(result["screenshot"], sub_output_dir / "screenshot.png")
+                logger.success(f"Scraped {result['url']}, links count: {len(links)}")
             else:
                 logger.success(
-                    f"Scraped {result['url']}, links count: {len(all_links)}, screenshot: not taken"
+                    f"Scraped {result['url']}, links count: {len(links)}, screenshot: not taken"
                 )
 
-            html_path = os.path.join(html_files_dir, f"{safe_filename}.html")
-            with open(html_path, "w", encoding="utf-8") as f:
-                f.write(result["html"])
-            logger.success(f"Saved HTML for {result['url']} to: {html_path}")
-            html_list.append(result["html"])
+            html_list.append(html)
     duration = time.perf_counter() - start
     logger.info(f"Done sync scraped {len(html_list)} htmls in {duration:.2f} seconds")
 
--- scrapers/run_format_html.py
+++ scrapers/run_format_html.py
@@ -30,9 +30,9 @@ from jet.transformers.formatters import format_html
 
 if __name__ == "__main__":
     # html_file = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/scrapers/node_extraction/sample.html"
-    html_file = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/jet_python_modules/jet/libs/smolagents/tools/examples/generated/examples_visit_webpage_tool/visit_webpage_tool_logs/tool_visit_webpage/call_0003/page.html"
     # html_file = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/search/playwright/generated/run_playwright_extract/top_isekai_anime_2025/https_gamerant_com_new_isekai_anime_2025/page.html"
     # html_file = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/search/playwright/generated/run_playwright_extract/top_rag_context_engineering_tips_2025_reddit/https_www_reddit_com_r_rag_comments_1mvzwrq_context_engineering_for_advanced_rag_curious_how/page.html"
+    html_file = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/scrapers/playwright/generated/run_scrape_urls_playwright/missav_ws_en_aed_137/sync_results/page.html"
     html_dir = os.path.dirname(html_file)
     output_dir = os.path.join(
         os.path.dirname(__file__),
