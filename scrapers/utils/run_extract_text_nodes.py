7ed51fb82c9ac7be824dc0509c7e429f165ec2ed
Jethro Estrada <jethroestrada237@gmail.com>
2026-08-01 11:34:14 +0800

    Updates

--- scrapers/utils/run_extract_text_nodes.py
+++ scrapers/utils/run_extract_text_nodes.py
@@ -1,25 +1,21 @@
 import os
 import shutil
-from jet.code.html_utils import convert_dl_blocks_to_md, preprocess_html
-from jet.file.utils import load_file, save_file
+
+from jet.file.utils import save_file
 from jet.scrapers.text_nodes import extract_text_nodes
 
 OUTPUT_DIR = os.path.join(
-    os.path.dirname(__file__), "generated", os.path.splitext(os.path.basename(__file__))[0])
+    os.path.dirname(__file__),
+    "generated",
+    os.path.splitext(os.path.basename(__file__))[0],
+)
 shutil.rmtree(OUTPUT_DIR, ignore_errors=True)
 
 if __name__ == "__main__":
-    html_file = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/search/playwright/generated/run_playwright_extract/top_rag_context_engineering_tips_2025_reddit/https_www_reddit_com_r_rag_comments_1mvzwrq_context_engineering_for_advanced_rag_curious_how/page.html"
+    link = "https://docs.tavily.com/documentation/api-reference/endpoint/crawl"
 
-    output_dir = OUTPUT_DIR
-
-    html_str: str = load_file(html_file)
-    html_str = convert_dl_blocks_to_md(html_str)
-    html_str = preprocess_html(html_str, excludes=["head", "nav", "footer", "script", "style", "button"])
-    save_file(html_str, f"{output_dir}/page.html")
-
-    text_nodes = extract_text_nodes(html_str)
-    save_file(text_nodes, f"{output_dir}/text_nodes.json")
+    text_nodes = extract_text_nodes(link)
+    save_file(text_nodes, f"{OUTPUT_DIR}/text_nodes.json")
 
     text_elements = [dict(node.get_element_details()) for node in text_nodes]
-    save_file(text_elements, f"{output_dir}/text_elements.json")
+    save_file(text_elements, f"{OUTPUT_DIR}/text_elements.json")
---
+++ scrapers/utils/run_extract_text_nodes_html.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+
+from jet.code.html_utils import convert_dl_blocks_to_md, preprocess_html
+from jet.file.utils import load_file, save_file
+from jet.scrapers.text_nodes import extract_text_nodes
+
+OUTPUT_DIR = os.path.join(
+    os.path.dirname(__file__),
+    "generated",
+    os.path.splitext(os.path.basename(__file__))[0],
+)
+shutil.rmtree(OUTPUT_DIR, ignore_errors=True)
+
+if __name__ == "__main__":
+    html_file = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/search/playwright/generated/run_playwright_extract/top_rag_context_engineering_tips_2025_reddit/https_www_reddit_com_r_rag_comments_1mvzwrq_context_engineering_for_advanced_rag_curious_how/page.html"
+
+    html_str: str = load_file(html_file)
+    html_str = convert_dl_blocks_to_md(html_str)
+    html_str = preprocess_html(
+        html_str, excludes=["head", "nav", "footer", "script", "style", "button"]
+    )
+    save_file(html_str, f"{OUTPUT_DIR}/page.html")
+
+    text_nodes = extract_text_nodes(html_str)
+    save_file(text_nodes, f"{OUTPUT_DIR}/text_nodes.json")
+
+    text_elements = [dict(node.get_element_details()) for node in text_nodes]
+    save_file(text_elements, f"{OUTPUT_DIR}/text_elements.json")
