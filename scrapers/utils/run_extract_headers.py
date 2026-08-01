e957255ebd7fe50ca184b9a0cc556c16ac1a7ea6
Jethro Estrada <jethroestrada237@gmail.com>
2026-08-01 12:12:18 +0800

    Updates

--- scrapers/utils/run_extract_headers.py
+++ scrapers/utils/run_extract_headers.py
@@ -1,22 +1,19 @@
 import os
 import shutil
 from typing import List
-from jet.code.html_utils import convert_dl_blocks_to_md
-from jet.file.utils import load_file, save_file
+
+from jet.file.utils import save_file
 from jet.scrapers.header_hierarchy import HtmlHeaderDoc, extract_header_hierarchy
 
 OUTPUT_DIR = os.path.join(
-    os.path.dirname(__file__), "generated", os.path.splitext(os.path.basename(__file__))[0])
+    os.path.dirname(__file__),
+    "generated",
+    os.path.splitext(os.path.basename(__file__))[0],
+)
 shutil.rmtree(OUTPUT_DIR, ignore_errors=True)
 
 if __name__ == "__main__":
-    html_file = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/search/playwright/generated/run_playwright_extract/top_isekai_anime_2025/https_gamerant_com_new_isekai_anime_2025/page.html"
+    link = "https://docs.tavily.com/documentation/api-reference/endpoint/crawl"
 
-    output_dir = OUTPUT_DIR
-
-    html_str: str = load_file(html_file)
-    html_str = convert_dl_blocks_to_md(html_str)
-    save_file(html_str, f"{output_dir}/page.html")
-
-    headings: List[HtmlHeaderDoc] = extract_header_hierarchy(html_str)
+    headings: List[HtmlHeaderDoc] = extract_header_hierarchy(link)
     save_file(headings, f"{OUTPUT_DIR}/headings.json")
