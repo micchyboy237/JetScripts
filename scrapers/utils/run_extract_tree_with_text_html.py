7aebf09ad468c396f54e3d865d5c0bc3da10d52b
Jethro Estrada <jethroestrada237@gmail.com>
2026-08-01 11:32:17 +0800

    Updates

--- scrapers/utils/run_extract_tree_with_text_html.py
+++ scrapers/utils/run_extract_tree_with_text_html.py
@@ -1,7 +1,8 @@
 import os
 import shutil
 
-from jet.file.utils import save_file
+from jet.code.html_utils import convert_dl_blocks_to_md, preprocess_html
+from jet.file.utils import load_file, save_file
 from jet.scrapers.utils import extract_tree_with_text, flatten_tree_to_base_nodes
 
 OUTPUT_DIR = os.path.join(
@@ -12,10 +13,17 @@ OUTPUT_DIR = os.path.join(
 shutil.rmtree(OUTPUT_DIR, ignore_errors=True)
 
 if __name__ == "__main__":
-    link = "https://docs.tavily.com/documentation/api-reference/endpoint/crawl"
+    html_file = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/search/playwright/generated/run_playwright_extract/top_rag_context_engineering_tips_2025_reddit/https_www_reddit_com_r_rag_comments_1mvzwrq_context_engineering_for_advanced_rag_curious_how/page.html"
+
+    html_str: str = load_file(html_file)
+    html_str = convert_dl_blocks_to_md(html_str)
+    html_str = preprocess_html(
+        html_str, excludes=["head", "nav", "footer", "script", "style", "button"]
+    )
+    save_file(html_str, f"{OUTPUT_DIR}/page.html")
 
     # Get the tree-like structure
-    tree_elements = extract_tree_with_text(link)
+    tree_elements = extract_tree_with_text(html_str)
     save_file(tree_elements, f"{OUTPUT_DIR}/tree_elements.json")
 
     all_nodes = flatten_tree_to_base_nodes(tree_elements)
---
+++ wordnet/analyzers/run_analyze_pos_tags_ai_jobs.py
@@ -0,0 +1,41 @@
+import os
+import shutil
+
+from jet.file.utils import save_file
+from jet.libs.bertopic.examples.mock import load_sample_jobs_ai_llm_python
+from jet.wordnet.analyzers.analyze_pos_tags import analyze_pos_tags
+
+
+def main_pos_tagger(texts, output_dir):
+    shutil.rmtree(output_dir, ignore_errors=True)
+    os.makedirs(output_dir, exist_ok=True)
+
+    print(f"Number of texts: {len(texts)}")
+    save_file(texts, f"{output_dir}/texts.json")
+
+    texts = [{"text": t, "lang": "en"} for t in texts]
+    includes_pos = ['PROPN', 'NOUN', 'ADJ', 'VERB']
+    top_n = 50
+
+    analyze_pos_tags(texts, n=2, from_start=True,
+                     words_only=True, top_n=top_n, includes_pos=includes_pos, output_dir=output_dir)
+    analyze_pos_tags(texts, n=3, from_start=True,
+                     words_only=True, top_n=top_n, includes_pos=includes_pos, output_dir=output_dir)
+    excludes_pos = ['[BOS]', '[EOS]']
+    analyze_pos_tags(texts, n=1, from_start=False,
+                     words_only=True, top_n=top_n, includes_pos=includes_pos, output_dir=output_dir)
+    analyze_pos_tags(texts, n=2, from_start=False,
+                     words_only=True, top_n=top_n, includes_pos=includes_pos, output_dir=output_dir)
+    analyze_pos_tags(texts, n=1, from_start=False,
+                     excludes_pos=excludes_pos, top_n=top_n, includes_pos=includes_pos, output_dir=output_dir)
+    analyze_pos_tags(texts, n=2, from_start=False,
+                     excludes_pos=excludes_pos, top_n=top_n, includes_pos=includes_pos, output_dir=output_dir)
+
+if __name__ == '__main__':
+    output_dir = os.path.join(
+        os.path.dirname(__file__), "generated", os.path.splitext(os.path.basename(__file__))[0])
+
+    texts = load_sample_jobs_ai_llm_python()
+
+    sub_output_dir = f"{output_dir}/jobs"
+    main_pos_tagger(texts, sub_output_dir)
