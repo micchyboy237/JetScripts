da1889ec00d415ece2ccef5046f9d7b88d2f031d
Jethro Estrada <jethroestrada237@gmail.com>
2026-08-01 14:28:31 +0800

    Updates

--- wordnet/analyzers/run_analyze_ner.py
+++ wordnet/analyzers/run_analyze_ner.py
@@ -6,11 +6,12 @@ from jet.libs.bertopic.examples.mock import load_sample_jobs_ai_llm_python
 from jet.transformers.object import make_serializable
 from jet.wordnet.analyzers.analyze_ner import analyze_ner
 
-if __name__ == "__main__":
-    OUTPUT_DIR = Path(__file__).parent / "generated" / Path(__file__).stem
-    shutil.rmtree(OUTPUT_DIR, ignore_errors=True)
-    OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+OUTPUT_DIR = Path(__file__).parent / "generated" / Path(__file__).stem
+shutil.rmtree(OUTPUT_DIR, ignore_errors=True)
+OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
+
+if __name__ == "__main__":
     texts = load_sample_jobs_ai_llm_python()
 
     result = analyze_ner(texts)
