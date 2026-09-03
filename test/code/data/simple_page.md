042fb15b164ec60b07ec05a126853820d0378e21
Jethro Estrada <jethroestrada237@gmail.com>
2026-09-03 23:45:03 +0800

    Updates

---
+++ test/code/data/simple_page.md
@@ -0,0 +1,1 @@
+from jet.code.markdown_utils.\_converters import convert_html_to_markdown
---
+++ test/code/run_get_md_header_docs.py
@@ -0,0 +1,13 @@
+import shutil
+from pathlib import Path
+
+# Rich console for styled resource links
+from rich.console import Console
+
+console = Console()
+
+OUTPUT_DIR = Path(__file__).parent / "generated" / Path(__file__).stem
+shutil.rmtree(OUTPUT_DIR, ignore_errors=True)
+OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+
+DATA_PATH = "/Users/jethroestrada/Desktop/External_Projects/Jet_Projects/JetScripts/test/code/data/simple_page.md"
